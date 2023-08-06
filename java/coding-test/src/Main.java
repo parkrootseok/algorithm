fe14b6230913492e1bcb05e4d648401562dd6fbb
parkrootseok <parkrootseok@gmail.com>
2023-08-06 17:07:42 +0900

    Feat : Section 3 - 2

--- java/coding-test/src/Main.java
+++ java/coding-test/src/Main.java
@@ -1,14 +1,18 @@
 import sec01.application.Sec01Application;
 import sec02.application.Sec02Application;
+import sec03.application.Sec03Application;
 
 public class Main {
     public static void main(String[] args) {
 
         Sec01Application sec01 = new Sec01Application();
         Sec02Application sec02 = new Sec02Application();
+        Sec03Application sec03 = new Sec03Application();
 
         // sec01.ex12();
-        sec02.ex12  ();
+        // sec02.ex12();
+        sec03.ex06();
+
 
     }
 }
