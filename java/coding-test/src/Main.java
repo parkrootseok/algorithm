d4866fe651f069d5e27f33f17f11688b0e7fd87e
parkrootseok <parkrootseok@gmail.com>
2023-08-10 21:32:07 +0900

    Feat : Section 5
    - 올바른 괄호
    - 괄호 문자 제거
    - 크레인 인형 뽑기
    - 후위식 연산
    - 쇠막대기
    - 공주 구하기
    - 교육과정 설계
    - 응급실

--- java/coding-test/src/Main.java
+++ java/coding-test/src/Main.java
@@ -2,6 +2,7 @@ import sec01.application.Sec01Application;
 import sec02.application.Sec02Application;
 import sec03.application.Sec03Application;
 import sec04.application.Sec04Application;
+import sec05.application.Sec05Application;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,11 +11,14 @@ public class Main {
         Sec02Application sec02 = new Sec02Application();
         Sec03Application sec03 = new Sec03Application();
         Sec04Application sec04 = new Sec04Application();
+        Sec05Application sec05 = new Sec05Application();
 
         // sec01.ex12();
         // sec02.ex12();
         // sec03.ex06();
-        sec04.ex05();
+        // sec04.ex05();
+        sec05.ex08();
+
 
     }
 }
