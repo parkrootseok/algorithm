99600b17daf47309d4c15ecd2fa74b3bf48bfd38
parkrootseok <parkrootseok@gmail.com>
2023-09-04 23:03:49 +0900

    Rename: 파일 이동

--- java/coding-test/src/baekjoon/class1/p8958/Main.java
+++ java/coding-test/src/baekjoon/class1/p8958/Main.java
@@ -1,11 +1,10 @@
-package baekjoon.p10809;
+package baekjoon.class1.p8958;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,26 +12,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String str = br.readLine();
+        int N = Integer.parseInt(br.readLine());
 
-        int[] numbers = new int[26];
-        Arrays.fill(numbers, -1);
+        for (int i = 0 ; i < N ; i++) {
 
-        for (int i = 0 ; i < str.length() ; i++) {
+            String str = br.readLine();
 
-            int idx = str.charAt(i) - 'a';
+            int sum = 0, cnt = 0;
+            for (char c : str.toCharArray()) {
 
-            if (numbers[idx] == -1) {
-                numbers[idx] = i;
+                if (c == 'X') {
+                    cnt = 0;
+                }
+
+                if (c == 'O') {
+                    cnt++;
+                }
+
+                sum += cnt;
+
             }
 
+            bw.write(sum + "\n");
+
         }
 
-        for (int n : numbers) {
-            bw.write(n + " ");
-        }
-
-        bw.newLine();
         bw.close();
 
     }
