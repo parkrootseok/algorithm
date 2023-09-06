40576f4ea8b35a63843923bc567b600f0d4b505c
parkrootseok <parkrootseok@gmail.com>
2023-09-06 22:40:05 +0900

    Chore: .gitignore 수정 변경

--- java/coding-test/src/Main.java
+++ java/coding-test/src/Main.java
@@ -1,26 +1,22 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 public class Main {
+
+    public int[] solution(int n) {
+        return IntStream.rangeClosed(0, n).filter(v -> v % 2 == 1).toArray();
+    }
+
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Main m = new Main();
 
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), "");
-
-        int sum = 0;
-        while (st.hasMoreTokens()) {
-            sum += Integer.parseInt(st.nextToken());
-        }
-
-        bw.write(sum);
-        bw.close();
+        int[] arr = m.solution(15);
+        System.out.println();
 
     }
 
