098d53ad981856dd7b1e8452731a324ab8870ea0
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-09-26 18:45:21 +0900

    [unrated] Title: 배열 만들기 6, Time: 14.10 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < arr.length; ) {
+
+            if (result.size() == 0) {
+                result.add(arr[i++]);
+            } else if (result.get(result.size() - 1) == arr[i]) {
+                result.remove(result.size() - 1);
+                i++;
+            } else {
+                result.add(arr[i++]);
+            }
+
+
+        }
+
+        int[] answer = result.stream().mapToInt(Integer::valueOf).toArray();
+        if (answer.length == 0) {
+            return new int[]{-1};
+        }
+
+        return answer;
+
+    }
+}
