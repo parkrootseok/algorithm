8e292e764b44562ed00626389a2208e40fc4279d
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-09-26 18:15:57 +0900

    [unrated] Title: 세 개의 구분자, Time: 57.34 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -4,15 +4,17 @@ class Solution {
      public String[] solution(String myStr) {
 
         String[] result = myStr.split("[a-c]");
-        result = Arrays.stream(result)
-                .filter(str -> !str.isEmpty())
-                .toArray(String[]::new);
 
         if (result.length == 0) {
             return new String[]{"EMPTY"};
         }
+        
+        result = Arrays.stream(result)
+                .filter(str -> !str.isEmpty())
+                .toArray(String[]::new);
 
         return result;
 
     }
+
 }
