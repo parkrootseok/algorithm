31f40d6cdf1c0f7bca7c25a633931aa1a4a463b7
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-09-27 19:29:32 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.56 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,37 +1,10 @@
 import java.util.*;
 
 class Solution {
-      public int solution(int[] numbers) {
+    public int solution(int[] numbers) {
 
-        ArrayList<Integer> positive = new ArrayList<>();
-        ArrayList<Integer> negative = new ArrayList<>();
-
-        for (int n : numbers) {
-
-            if (n >= 0) {
-                positive.add(n);
-            }
-
-            if (n <= 0) {
-                negative.add(n);
-            }
-
-        }
-
-        Collections.sort(positive);
-        Collections.sort(negative);
-        
-        if (positive.size() < 2 && negative.size() < 2) {
-            return positive.get(0) * negative.get(0);
-        }  else if (negative.size() < 2) {
-            return positive.get(positive.size() - 1) * positive.get(positive.size() - 2);
-        } else if (positive.size() < 2) {
-            return negative.get(negative.size() - 1) * negative.get(negative.size() - 2);
-        } else {
-            int pos = positive.get(positive.size() - 1) * positive.get(positive.size() - 2), neg = negative.get(negative.size() - 1) * negative.get(negative.size() - 2);
-            return pos > neg ? pos : neg;
-        }
+        Arrays.sort(numbers);
+        return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 2] * numbers[numbers.length - 1]);
         
     }
-    
 }
