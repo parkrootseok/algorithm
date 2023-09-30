5cb0f71730b43f36ed956fdd88c00007a0687724
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-09-30 13:49:44 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String n_str) {
+
+        int idx = 0;
+        while (n_str.charAt(idx) == '0') {
+            idx++;
+        }
+
+        return n_str.substring(idx);
+
+    }
+}
