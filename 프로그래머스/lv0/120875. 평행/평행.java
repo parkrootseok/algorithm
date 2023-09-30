f7f944ac8a838869142e3810a606788c8304d640
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-09-30 23:56:03 +0900

    [level 0] Title: 평행, Time: 0.10 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -2,17 +2,16 @@ import java.util.*;
 
 class Solution {
     
-     public double gradient(int x1, int y1, int x2, int y2) {
+    public double gradient(int x1, int y1, int x2, int y2) {
 
-        return (y1 - y2) / (double) (x1 - x2);
+        return (y2 - y1) / (double) (x2 - x1);
 
     }
 
     public int solution(int[][] dots) {
 
-//        ArrayList<line> gradients = new ArrayList<>();
-        HashMap<Double, Double> map = new HashMap<>();
-        
+        HashMap<Double, Double> gradients = new HashMap<>();
+
         for (int i = 0; i < dots.length; i++) {
 
             int x1 = dots[i][0], y1 = dots[i][1];
@@ -22,25 +21,15 @@ class Solution {
                 double a = gradient(x1, y1, dots[j][0], dots[j][1]);
                 double b = y1 - a * x1;
 
-                if (map.get(a) != null && (map.get(a) != b || map.get(a) == 0)) {
+                if (gradients.containsKey(a) && (gradients.get(a) != b || gradients.get(a) == 0)) {
                     return 1;
                 }
 
-                map.put(a, b);
+                gradients.put(a, b);
 
             }
 
         }
-
-//        Collections.sort(gradients);
-//
-//        for (int i = 0; i < gradients.size() - 1; i++) {
-//
-//            if (gradients.get(i).gradient == gradients.get(i + 1).gradient) {
-//                return 1;
-//            }
-//
-//        }
 
         return 0;
 
