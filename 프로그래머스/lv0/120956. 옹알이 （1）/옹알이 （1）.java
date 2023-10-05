8b7606dbd13a6c7ae169f8e49b8997c53f3c94f0
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-10-05 19:13:01 +0900

    [level 0] Title: 옹알이 (1), Time: 1.86 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,23 +1,12 @@
 import java.util.*;
 
 class Solution {
-      public int solution(String[] babbling) {
+    public int solution(String[] babbling) {
 
-        String[] possible = new String[]{"aya", "ye", "woo", "ma"};
-        int cnt = 0;
-        for (String b : babbling) {
-
-            for (String p : possible) {
-                b = b.replace(p, " ");
-            }
-
-            if (b.replace(" ", "").length() == 0) {
-                cnt++;
-            }
-
-        }
-
-        return cnt;
+        return (int) Arrays.stream(babbling)
+                .filter(b -> b.matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$"))
+                .count();
 
     }
+
 }
