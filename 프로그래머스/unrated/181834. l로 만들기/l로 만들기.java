a9653cd95bd94f8d461c68e49d397ac26178cc6c
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-10-06 15:39:32 +0900

    [unrated] Title: l로 만들기, Time: 22.08 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,18 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0 ; i < myString.length() ; i++) {
-                
-            if (myString.charAt(i) < 'l') {
-                sb.append("l");
-            } else {
-                sb.append(myString.charAt(i));
-            }
-            
-        }
-        
-        return sb.toString();
-        
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
