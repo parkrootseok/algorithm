51c855c2c9fa6832d3351a1a21f94d0e1fb0b5b1
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-10-07 19:35:06 +0900

    [level 0] Title: k의 개수, Time: 1.89 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,21 +1,23 @@
 class Solution {
-      public int solution(int i, int j, int k) {
+    public int solution(int i, int j, int k) {
 
         int answer = 0;
-
+       
         for (int start = i ; start <= j ; start++) {
-
-            String s = String.valueOf(start);
-
-            for (int pos = 0 ; pos < s.length() ; pos++) {
-                if (s.charAt(pos) == k + '0') {
+            
+            int tmp = start;
+            
+            while (tmp > 0) {
+                if (k == tmp % 10) {
                     answer++;
                 }
+
+                tmp /= 10;
             }
-
+            
         }
-
+        
         return answer;
-
+    
     }
 }
