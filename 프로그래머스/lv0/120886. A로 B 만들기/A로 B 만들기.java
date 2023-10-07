d4d06fdfd2a7c07e4486e67ec746c5a96cda4fc8
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-10-07 19:24:15 +0900

    [level 0] Title: A로 B 만들기, Time: 0.50 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String before, String after) {
+
+        char[] be = before.toCharArray();
+        char[] af  = after.toCharArray();
+        
+        Arrays.sort(be);
+        Arrays.sort(af);
+        
+        if (Arrays.equals(be, af)) {
+            return 1;
+        }
+        
+        return 0;
+
+    }
+}
