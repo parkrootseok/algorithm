c90759b1a0b73eed59e01e9339736216e1e5033b
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-10-26 23:25:23 +0900

    [D2] Title: 시각 덧셈, Time: 132 ms, Memory: 20,412 KB -BaekjoonHub

--- SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
@@ -2,38 +2,46 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 class Solution {
 
     static int N, K;
-    
+
     public static void main(String args[]) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        SimpleDateFormat format = new SimpleDateFormat("hh mm");
 
         int T = Integer.parseInt(br.readLine());
-        
+
         for (int i = 1; i <= T; i++) {
-            
+
             bw.write("#" + i + " ");
             String[] time = br.readLine().split(" ");
-            
-            int m = Integer.parseInt(time[1]) + Integer.parseInt(time[3]);
-            int h = Integer.parseInt(time[0]) + Integer.parseInt(time[2]);
-            
-            if (m >= 60) {
-                m -= 60;
-                h++;
+
+            Calendar calendarA = Calendar.getInstance();
+            calendarA.setTime(format.parse(time[0] + " " + time[1]));
+
+            Calendar calendarB = Calendar.getInstance();
+            calendarB.setTime(format.parse(time[2] + " " + time[3]));
+
+            calendarA.add(Calendar.HOUR, calendarB.get(Calendar.HOUR));
+            calendarA.add(Calendar.MINUTE, calendarB.get(Calendar.MINUTE));
+
+            int h;
+            if (calendarA.get(Calendar.HOUR_OF_DAY) > 12) {
+                h = calendarA.get(Calendar.HOUR_OF_DAY) - 12;
+            } else {
+                h = calendarA.get(Calendar.HOUR_OF_DAY);
             }
-            
-            if (h > 12) {
-                h -= 12;
-            }
-            
-            bw.write(h + " " + m);
+
+            bw.write(h + " " + calendarA.get(Calendar.MINUTE));
             bw.write("\n");
-            
+
         }
 
         bw.close();
