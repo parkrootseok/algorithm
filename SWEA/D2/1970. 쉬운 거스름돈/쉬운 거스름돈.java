f8f455d9219d230be7947613e723cfd67b2edc2a
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-10-27 22:36:54 +0900

    [D2] Title: 쉬운 거스름돈, Time: 97 ms, Memory: 19,168 KB -BaekjoonHub

---
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+
+    static int N, K;
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] money = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10};
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            bw.write("#" + i + "\n");
+
+            N = Integer.parseInt(br.readLine());
+
+
+            for (int j = 0 ; j < money.length ; j++) {
+                int cnt = 0;
+                while (N >= money[j]) {
+                    cnt = N / money[j];
+                    N %= money[j];
+                }
+
+                bw.write(cnt + " ");
+
+            }
+
+            bw.write("\n");
+
+        }
+
+        bw.close();
+
+    }
+
+}
