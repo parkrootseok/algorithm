718f2c4d2d655ba656ff1bff838941218db43ec8
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-01-31 14:10:03 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1012 ms, Memory: 159380 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -13,8 +13,9 @@ import java.util.List;
  * - NbyN 크기의 표에서 (x1, y1) 부터 (x2, y2)까지 합을 구해라
  * 
  * 1. N과 M을 받는다.
- * 2. N*N개의 수를 입력 받는다.
- * 3. 시점(x1, y1)과 종점(x2, y2)을 입력받고 합을 구한다.
+ * 2. N*N개의 수를 입력 받은 후 행에 대한 누적합을 계산
+ * 3. 열에 대한 누적합을 계산
+ * 4. 시점(x1, y1)과 종점(x2, y2)을 입력받고 합을 구한다.
  * 		-> x1 ~ x2은 행 범위, y1 ~ y2은 열 범위가 됨
  * 
  */
@@ -37,7 +38,7 @@ class Main {
 		N = Integer.parseInt(inputs[0]);
 		M = Integer.parseInt(inputs[1]);
 
-		// 2. N*N개의 수를 입력 받는다.
+		// 2. N*N개의 수를 입력 받은 후 행에 대한 누적합을 계산
 		map = new int[N+1][N+1];
 		for(int row = 1; row <= N; row++) {
 			
@@ -47,8 +48,15 @@ class Main {
 			}
 		
 		}
-	
-		// 3. 시점(x1, y1)과 종점(x2, y2)을 입력받고 합을 구한다.
+		
+		// 3. 열에 대한 누적합을 계산
+		for(int row = 1; row <= N; row++) {
+			for(int cols = 1; cols <= N; cols++) {
+				map[row][cols] += map[row-1][cols]; 	
+			}		
+		}
+		
+		// 4. 시점(x1, y1)과 종점(x2, y2)을 입력받고 합을 구한다.
 		int x1, y1, x2, y2;
 		for(int curM = 0; curM < M; curM++) {
 			
@@ -59,13 +67,9 @@ class Main {
 			x2 = Integer.parseInt(inputs[2]);
 			y2 = Integer.parseInt(inputs[3]);
 			
-			// x1 ~ x2은 행 범위, y1 ~ y2은 열 범위
-			int sum = 0;
-			for(int curX = x1; curX <= x2; curX++) {
-				sum += map[curX][y2] - map[curX][y1 - 1];
-			}
-			
-			sb.append(sum).append("\n");
+			// (x2, y2) : x2, y2까지 모든 누적합
+			// 시작 좌표부터 (x2,y2)영역에서 범위를 벗어난 열, 행 누적합 제외 
+			sb.append(map[x2][y2] - map[x2][y1 - 1] - map[x1 - 1][y2] + map[x1 - 1][y1 - 1]).append("\n");
 			
 		}
 
