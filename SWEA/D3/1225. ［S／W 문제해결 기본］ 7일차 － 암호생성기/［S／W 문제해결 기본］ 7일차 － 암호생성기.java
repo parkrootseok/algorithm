23c1cf59d0a2b40721e0894d7a8b56182d136691
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-02-02 12:00:21 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 130 ms, Memory: 22,128 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -2,70 +2,94 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.stream.Collectors;
+
+/**
+ * SWEA_1225_암호생성기
+ * @author parkrootseok
+ * 
+ * - 8자리 암호 생성
+ * 
+ * 1. 테스트 케이스 번호 받고
+ * 2. 8자리 숫자 받고
+ * 3. 큐에 8자리 숫자 모두 offer
+ * 3. 암호 생성 실시
+ *  3-1. 큐에서 poll하고 반복 횟수만큼 감소
+ *  3-2. 감소한 숫자가 0이하이면 0을 삽입하고 종료
+ *  
+ */
 
 class Solution {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void generateSecret() {
 
-    static int N, K, ANSWER;
-    static Queue<Integer> numbers;
-    static int CYCLE = 6;
+		int order = 1;
+		while (true) {
 
-    public static void solution() {
+			// 3-1. 큐에서 poll하고 반복 횟수만큼 감소
+			int cur = numbers.poll() - order++;
 
-        int order = 1;
+			// 3-2. 감소한 숫자가 0이하이면 0을 삽입하고 종료
+			if (cur <= 0) {
+				numbers.offer(0);
+				return;
+			} else {
+				// 그렇지 않다면 삽입
+				numbers.offer(cur);
+			}
 
-        while (!numbers.contains(0)) {
+			// 한 싸이클을 수행할 때마다 초기화
+			if (order == CYCLE) {
+				order = 1;
+			}
 
-            int cur = numbers.poll() - order++;
+		}
 
-            if (cur <= 0) {
-                numbers.offer(0);
-            } else {
-                numbers.offer(cur);
-            }
+	}
 
-            if (order == CYCLE) {
-                order = 1;
-            }
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringBuilder sb;
+	static String[] inputs;
 
-        }
+	static int tcNumber;
+	static Queue<Integer> numbers;
+	static int CYCLE = 6;
 
-    }
+	public static void main(String args[]) throws Exception {
 
-    public static void main(String args[]) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
 
-//        int T = Integer.parseInt(br.readLine());
+		for (int curTC = 1; curTC <= 10; curTC++) {
 
-        for (int i = 1; i <= 10; i++) {
+			sb.append("#").append(curTC);
 
-            bw.write("#" + i);
+			// 1. 테스트 케이스 번호 받고
+			tcNumber = Integer.parseInt(br.readLine());
 
-            N = Integer.parseInt(br.readLine());
+			// 2. 8자리 숫자 받고 큐에 8자리 숫자 모두 offer
+			numbers = new ArrayDeque<>();
+			inputs = br.readLine().trim().split(" ");
+			for (String input : inputs) {
+				numbers.offer(Integer.parseInt(input));
+			}
 
-            numbers = new LinkedList<>();
-            String[] inputs = br.readLine().split(" ");
-            for (String input : inputs) {
-                numbers.add(Integer.parseInt(input));
-            }
+			// 3. 암호 생성 실시
+			generateSecret();
 
-            solution();
+			for (int number : numbers) {
+				sb.append(" ").append(number);
+			}
 
-            for (int number : numbers) {
-                bw.write(" " + number);
-            }
+			sb.append("\n");
 
-            bw.write("\n");
+		}
 
-        }
+		bw.write(sb.toString());
+		bw.close();
 
-        bw.close();
-
-    }
-
+	}
 }
