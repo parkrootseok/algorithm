cff71639aac50abccaf939fabc6b8c8fd824ce93
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-02-07 16:11:06 +0900

    [D4] Title: 정사각형 방, Time: 180 ms, Memory: 33,208 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -3,9 +3,6 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Queue;
 
 /**
@@ -21,15 +18,15 @@ import java.util.Queue;
  * 2. 방 크기 입력
  * 3. 방에 대한 입력
  * 4. 하나의 방에서 4방 탐색을 진행
- *  4-1. 다음 방으로 이동가능 하면 재귀 호출
+ *  4-1. 이동할 수 있는 방이라면 큐에 삽입
  * 5. 탐색한 방 수가 최대이면 초기화 후 위치 저장
  *  
  */
 
 class Solution {
 
-	static int[] dx = {1, -1, 0, 0};
-	static int[] dy = {0, 0, 1, -1};
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
 
 	static BufferedReader br;
 	static BufferedWriter bw;
@@ -40,12 +37,11 @@ class Solution {
 	static int roomSize;
 
 	static int[][] room;
-	static boolean[][] isVisted;
+	static boolean[][] isVisited;
 
 	static int maxMoveCount;
 	static int minRoomNumber;
-	static int moveCount;
-	
+
 	public static boolean isValid(int row, int cols) {
 
 		if (!(0 <= row && row < roomSize && 0 <= cols && cols < roomSize)) {
@@ -58,7 +54,7 @@ class Solution {
 
 	public static boolean isPossible(int curRoom, int nextRoom) {
 
-		int diff = nextRoom - curRoom;
+		int diff = Math.abs(nextRoom - curRoom);
 
 		if (diff == 1) {
 			return true;
@@ -69,27 +65,59 @@ class Solution {
 	}
 
 	public static void getMoveCount(int row, int cols) {
-		
-		int nextRow, nextCols;
-		for (int index = 0; index < dx.length; index++) {
 
-			nextRow = row + dx[index];
-			nextCols = cols + dy[index];
+		int moveCount = 0;
+		int minRoom = Integer.MAX_VALUE;
 
-			// 인덱스가 범위내에 존재하고
-			if (!isValid(nextRow, nextCols)) {
-				continue;
-			}
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {row, cols});
+		isVisited[row][cols] = true;
 
-			// 두 방의 차이가 정확이 1이면
-			if (!isPossible(room[row][cols], room[nextRow][nextCols])) {
-				continue;
-			}
+		while (!queue.isEmpty()) {
 
-			// 4-1. 다음 방으로 이동가능 하면 재귀 호출 방문 횟수 증가
+			int[] curRoom = queue.poll();
+
+			minRoom = Math.min(minRoom, room[curRoom[0]][curRoom[1]]);
 			moveCount++;
-			getMoveCount(nextRow, nextCols);
+			
+			// 4-1. 이동할 수 있는 방이라면 큐에 삽입
+			int nextRow, nextCols;
+			for (int index = 0; index < dx.length; index++) {
 
+				nextRow = curRoom[0] + dx[index];
+				nextCols = curRoom[1] + dy[index];
+
+				// 인덱스가 범위내에 존재하고
+				if (!isValid(nextRow, nextCols)) {
+					continue;
+				}
+
+				// 두 방의 차이가 정확이 1이고
+				if (!isPossible(room[curRoom[0]][curRoom[1]], room[nextRow][nextCols])) {
+					continue;
+				}
+
+				// 방문한적이 없다면
+				if (isVisited[nextRow][nextCols]) {
+					continue;
+				}
+
+				// 큐에 삽입 후 방문 표시 및 방문 횟수 카운트
+				isVisited[nextRow][nextCols] = true;
+				queue.offer(new int[] {nextRow, nextCols});
+				
+			}
+
+		}
+		
+		// 5. 탐색한 방 수가 최대이면 초기화 후 위치 저장
+		if (moveCount > maxMoveCount) {
+			minRoomNumber = minRoom;
+			maxMoveCount = moveCount;
+		}
+
+		else if (moveCount == maxMoveCount) {
+			minRoomNumber = Math.min(minRoomNumber, minRoom);
 		}
 
 	}
@@ -118,21 +146,36 @@ class Solution {
 			}
 
 			// 4. 하나의 방에서 4방 탐색을 진행
+			isVisited = new boolean[roomSize][roomSize];
 			maxMoveCount = Integer.MIN_VALUE;
 			minRoomNumber = roomSize * roomSize + 1;
 			for (int row = 0; row < roomSize; row++) {
 				for (int cols = 0; cols < roomSize; cols++) {
-					
-					moveCount = 1;
-					getMoveCount(row, cols); 
 
-					// 5. 탐색한 방 수가 최대이면 초기화 후 위치 저장
-					if (moveCount == maxMoveCount) {
-						minRoomNumber = Math.min(minRoomNumber, room[row][cols]);
-						maxMoveCount = moveCount;
-					} else if (moveCount > maxMoveCount) {
-						minRoomNumber = room[row][cols];
-						maxMoveCount = moveCount;
+					int nextRow, nextCols;
+					for (int index = 0; index < dx.length; index++) {
+
+						nextRow = row + dx[index];
+						nextCols = cols + dy[index];
+
+						// 인덱스가 범위내에 존재하고
+						if (!isValid(nextRow, nextCols)) {
+							continue;
+						}
+
+						// 두 방의 차이가 정확이 1이고
+						if (!isPossible(room[row][cols], room[nextRow][nextCols])) {
+							continue;
+						}
+
+						// 방문한적이 없다면
+						if (isVisited[nextRow][nextCols]) {
+							continue;
+						};
+
+						// 해당 위치부터 4방 탐색 시작
+						getMoveCount(row, cols);
+
 					}
 
 				}
