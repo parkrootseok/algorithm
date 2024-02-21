0ba4c5ec1bb9b99296c992d0ec1c1e586c9c77d5
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-02-21 13:53:45 +0900

    [D4] Title: 서로소 집합, Time: 480 ms, Memory: 114,416 KB -BaekjoonHub

--- SWEA/D4/3289. 서로소 집합/서로소 집합.java
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -2,10 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 /***
  * SWEA_3289_서로소집합
