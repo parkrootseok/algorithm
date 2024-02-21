8556751ea3a16ba57c3e36caabfc030908cf61d0
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-02-21 11:51:40 +0900

    [D4] Title: 서로소 집합, Time: 511 ms, Memory: 114,772 KB -BaekjoonHub

--- SWEA/D4/3289. 서로소 집합/서로소 집합.java
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -60,12 +60,18 @@ class Solution {
 		}
 		
 		// 아니라면 둘을 합집합 수행
-		if(rank[representationA] >= rank[representationB]) {
+		if(rank[representationA] > rank[representationB]) {
 			// A가 속한 집합의 랭크가 더 같거나 높은 경우 B를 A 밑으로
 			set[representationB] = set[representationA];
-		} else {
-			// B가 속한 집합의 랭크가 더 같거나 높은 경우 A를 B 밑으로
-			set[representationA] = set[representationB];
+			return;
+		} 
+		
+		// B가 속한 집합의 랭크가 더 같거나 높은 경우 A를 B 밑으로
+		set[representationA] = set[representationB];
+			
+		// 단, 두 합집합의 랭크가 같다면 밑으로 들어가게 될 집합의 랭크를 상승해야한다.
+		if(rank[representationA] == rank[representationB]) {
+			rank[representationB]++;
 		}
 		
 	}
