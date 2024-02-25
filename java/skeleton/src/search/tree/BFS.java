8e05fed3eb9e79bf02d307ea8cc7a038ec37927a
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-02-25 22:26:12 +0900

    refactor: search for Tree
    - BFS
    - DFS

--- java/skeleton/src/search/tree/BFS.java
+++ java/skeleton/src/search/tree/BFS.java
@@ -1,23 +1,21 @@
-package search;
+package search.tree;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
 
 /**
- * BFS
+ * BFS for Tree (트리 BFS 탐색)
  * @author parkrootseok
  *
  * 1. 큐에 첫 방문을 진행할 노드를 삽입
  * 3. 큐에 노드가 존재하지 않을 때 까지 반복
  *  3-1. 현재 가장 처음에 존재하는 노드를 꺼내고
  *  3-2. 큐에서 꺼낸 노드와 연결된 모든 노드를 탐색
- *   3-2-1. 방문할 노드가 이미 방문한 곳이라면 스킵
- *   3-2-2. 방문하지 않은 곳이라면 큐에 삽입 후 방문 처리
+ *   3-2-2. 큐에 삽입
  **/
 public class BFS {
 
-    static int TOTAL_NODE_NUMBER = 5;
-    boolean[] isVisited = new boolean[TOTAL_NODE_NUMBER];
+    static int linkedNode = 7;
 
     public static void main(String[] args) {
 
@@ -28,7 +26,7 @@ public class BFS {
         Queue<Integer> queue = new ArrayDeque<Integer>();
 
         // 1. 큐에 첫 방문을 진행할 노드를 삽입
-        queue.add();
+        queue.add(1);
 
         // 3. 큐에 노드가 존재하지 않을 때 까지 반복
         while (!queue.isEmpty()) {
@@ -39,14 +37,8 @@ public class BFS {
             // 3-2. 큐에서 꺼낸 노드와 연결된 모든 노드를 탐색
             for(int curNode = 0; curNode < linkedNode; curNode++) {
 
-                // 3-2-1. 방문할 노드가 이미 방문한 곳이라면 스킵
-                if(isVisited[curNode]) {
-                    continue;
-                }
-
-                // 3-2-2. 방문하지 않은 곳이라면 큐에 삽입 후 방문 처리
-                queue.add();
-                isVisited[curNode] = false;
+                // 3-2-2. 큐에 삽입
+                queue.add(curNode);
 
             }
 
---
+++ java/skeleton/src/search/tree/DFS.java
@@ -0,0 +1,33 @@
+package search.tree;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * DFS for Tree (트리 BFS 탐색)
+ * @author parkrootseok
+ *
+ * 1. 현재 방문한 노드와 연결된 노드를 탐색
+ *  1-1. 방문하지 않은 노드 방문
+ **/
+public class DFS {
+
+    static int linkedNode = 7;
+
+    public static void main(String[] args) {
+
+    }
+
+    public void dfs(int visitNode) {
+
+        // 1. 현재 방문한 노드와 연결된 노드를 탐색
+        for(int nextNode = 0; nextNode < linkedNode; nextNode++) {
+
+            // 1-1. 노드 방문
+            dfs(nextNode);
+
+        }
+
+    }
+
+}
