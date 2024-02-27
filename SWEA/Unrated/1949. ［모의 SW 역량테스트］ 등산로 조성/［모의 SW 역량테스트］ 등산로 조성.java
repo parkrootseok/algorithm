a4bde990dfa332298271ffd6b0f07cbb8ad18161
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-02-27 22:23:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 271 ms, Memory: 87,044 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -3,13 +3,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
 
 /**
  * SWEA_1949_등산로조성
