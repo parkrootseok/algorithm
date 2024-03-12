992ae5f444810a7cc3ae17e1f91da44e1821cf9a
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-03-12 17:11:03 +0900

    [level 1] Title: 최소직사각형, Time: 2.29 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,53 @@
+/**
+ * Programmers_최소직사각형
+ * @author parkrootseok
+ * 
+ * - 지갑
+ *   - 모든 명함을 담을 수 있어야함
+ *  
+ * - 명함
+ *  - 회전 가능
+ *  - 즉, 가로와 세로의 길이를 바꿀 수 있음 
+ *  
+ *  - 지갑에 모든 명함을 넣을 수 있고, 가장 작은 크기를 구해라
+ *  
+ * 1. 모든 변의 길이중 가장 짧고, 가장 긴 부분을 찾자
+ *  1-1. 명함의 너비와 높이에서 가장 짧고 긴 부분을 찾는다
+ *  1-2. 가장 긴 부분중 최대값과 가장 짧은 부분 중 최대갑을 찾는다
+ */
+public class Solution {
+	
+	static int cardNumber;
+	static int[][] cards;
+	static boolean[] isLotationed;
+	static int minSize;
+	
+    public int solution(int[][] sizes) {
+       
+    	cardNumber = sizes.length;
+    	cards = sizes;
+    	minSize = Integer.MAX_VALUE;
+    	
+    	int width = Integer.MIN_VALUE;
+		int height = Integer.MIN_VALUE;
+		
+		// 1. 모든 변의 길이중 가장 짧고, 가장 긴 부분을 찾자
+		for (int row = 0; row < cards.length; row++) {
+			
+			// 1-1. 명함의 너비와 높이에서 가장 짧고 긴 부분을 찾는다
+			int curMax = Math.max(cards[row][0], cards[row][1]);
+			int curMin = Math.min(cards[row][0], cards[row][1]);
+	
+			// 1-2. 가장 긴 부분중 최대값과 가장 짧은 부분 중 최대갑을 찾는다
+			width = Math.max(width, curMax);
+			height = Math.max(height, curMin);
+			
+		}
+		
+
+    	return width * height;
+        
+    }
+    
+    
+}
