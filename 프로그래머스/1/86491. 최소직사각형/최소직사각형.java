568beeaae12a4ce519348eda0dd372d00297ae20
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-03-12 17:13:44 +0900

    [level 1] Title: 최소직사각형, Time: 2.47 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -19,24 +19,21 @@ public class Solution {
 	
 	static int cardNumber;
 	static int[][] cards;
-	static boolean[] isLotationed;
-	static int minSize;
-	
+
     public int solution(int[][] sizes) {
        
     	cardNumber = sizes.length;
     	cards = sizes;
-    	minSize = Integer.MAX_VALUE;
     	
     	int width = Integer.MIN_VALUE;
 		int height = Integer.MIN_VALUE;
 		
 		// 1. 모든 변의 길이를 탐색
-		for (int row = 0; row < cards.length; row++) {
+		for (int curCard = 0; curCard < cardNumber; curCard++) {
 			
 			// 1-1. 명함의 너비와 높이에서 가장 길고 짧은 부분을 찾는다
-			int curMax = Math.max(cards[row][0], cards[row][1]);
-			int curMin = Math.min(cards[row][0], cards[row][1]);
+			int curMax = Math.max(cards[curCard][0], cards[curCard][1]);
+			int curMin = Math.min(cards[curCard][0], cards[curCard][1]);
 	
 			// 1-2. 가장 긴 부분 중 최대값과 가장 짧은 부분 중 최대값을 찾는다
 			width = Math.max(width, curMax);
