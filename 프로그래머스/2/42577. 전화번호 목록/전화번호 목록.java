3a364ab29f69fa3176814aa6321b101188e1545b
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-03-19 21:48:42 +0900

    [level 2] Title: 전화번호 목록, Time: 327.95 ms, Memory: 198 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -8,7 +8,11 @@ import java.util.HashMap;
  * - 전화번보후에서 한 번호가 다른 번호의 접두어인 경우를 확인
  *   - 있다면 false, 없다면 true를 리턴
  *
- *
+ * 1. 모든 전화번호를 해시맵에 저장
+ * 2. 해시맵에 저장된 전화번호 중 동일한 부분 문자열이 있는지 확인
+ * 	2-1. 현재 문자열의 모든 부분 문자열을 만들어 이를 포함하는 키값이 있는지 확인
+ * 		2-1-1. 0 ~ length 길이의 부분 문자열을 생성
+ * 		2-1-2. 부분 문자열과 같은 키를 가지는 문자열이 있는지 확인
  **/
 
 public class Solution {
@@ -17,14 +21,29 @@ public class Solution {
 
 		boolean answer = true;
 
-		Arrays.sort(phone_book);
+		// 1. 모든 전화번호를 해시맵에 저장
 		HashMap<String, Integer> set = new HashMap<>();
+		for (String tel : phone_book) {
+			set.put(tel, 0);
+		}
 
-		for (int curStringIdx = 0; curStringIdx < phone_book.length - 1; curStringIdx++) {
+		// 2. 해시맵에 저장된 전화번호 중 동일한 부분 문자열이 있는지 확인
+		for (int curStringIdx = 0; curStringIdx < phone_book.length; curStringIdx++) {
 
-			if (phone_book[curStringIdx + 1].startsWith(phone_book[curStringIdx])) {
-				answer = false;
-				break;
+			// 2-1. 현재 문자열의 모든 부분 문자열을 만들어 이를 포함하는 키값이 있는지 확인
+			for (int length = 1; length < phone_book[curStringIdx].length(); length++) {
+
+				// 2-1-1. 0 ~ length 길이의 부분 문자열을 생성
+				String prefix = phone_book[curStringIdx].substring(0, length);
+
+				// 2-1-2. 부분 문자열과 같은 키를 가지는 문자열이 있는지 확인
+				if (set.containsKey(prefix)) {
+
+					// 있다면 false로 변경 후 반복문 종료
+					answer = false;
+					break;
+				}
+
 			}
 
 		}
