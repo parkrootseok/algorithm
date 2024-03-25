adbe4e04df0d385231937634f0968d93c86dff6b
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-03-25 22:01:43 +0900

    [level 2] Title: 기능개발, Time: 0.31 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -8,43 +8,33 @@ import java.util.*;
  *  -> 우선 순위가 높은 기능을 배포할 때 우선 순위가 낮은 기능도 같이 배포
  * - 배포가 이루어질 때 마다 몇개의 배포가 이루어지는지 확인
  *
- * 1. 기능을 완수하기 위해 걸리는 날짜를 미리 계산
- * 2. 큐에 삽입된 예상 완료 기간을 탐색
- *   2-1. 현재 큐에서 가장 우선순위가 높은 원소가 현재 완료 기간보다 짧다면 배포
- *   2-2. 배포된 기능에 대한 개수를 기록
+ * 1. 기능을 완수하기 위해 걸리는 시간을 계산
+ * 2. 현재 필요한 기간이 큐에 존재하는 우선순위가 가장 높은 원소보다 크다면 큐 초기화 및 개수를 저장
  */
 public class Solution {
 
 	public int[] solution(int[] progresses, int[] speeds) {
 
-		// 1. 기능을 완수하기 위해 걸리는 날짜를 미리 계산
+		List<Integer> deployCount = new ArrayList<>();
 		Queue<Integer> workQ = new ArrayDeque<>();
 		for (int idx = 0; idx < progresses.length; idx++) {
 
-			int completeProgress = 100;
+			// 1. 기능을 완수하기 위해 걸리는 시간을 계산
 			float remainProgress = 100 - progresses[idx];
 			int needDay = (int) Math.ceil(remainProgress / speeds[idx]);
 
-			workQ.add(needDay);
-
-		}
-
-		// 2. 큐에 삽입된 예상 완료 기간을 탐색
-		List<Integer> deployCount = new ArrayList<>();
-		while (!workQ.isEmpty()) {
-
-			int curSize = workQ.size();
-			int curWorkNeedDay = workQ.poll();
-
-			// 2-1. 현재 큐에서 가장 우선순위가 높은 원소가 현재 완료 기간보다 짧거나 같다면 배포
-			while (!workQ.isEmpty() && workQ.peek() <= curWorkNeedDay) {
-				workQ.poll();
+			// 2. 현재 필요한 기간이 큐에 존재하는 우선순위가 가장 높은 원소보다 크다면 큐 초기화 및 개수를 저장
+			// 현재 우선순위가 가장 높은 기능보다 필요한 기간이 길다는 것은 다른 기간에 배포된다는 것을 의미함
+			if (!workQ.isEmpty() && workQ.peek() < needDay) {
+				deployCount.add(workQ.size());
+				workQ.clear();
 			}
 
-			// 2-2. 배포된 기능 개수(기존 Queue Size - 현재 Queue Size)를 기록
-			deployCount.add(curSize - workQ.size());
+			workQ.offer(needDay);
 
 		}
+
+		deployCount.add(workQ.size());
 
 		int[] result = new int[deployCount.size()];
 		for (int idx = 0; idx < deployCount.size(); idx++) {
