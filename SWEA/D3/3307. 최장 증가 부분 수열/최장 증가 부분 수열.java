ea7eab5ae903c2c44406cfa03b94103fcc77adf5
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-03-27 12:07:43 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 189 ms, Memory: 35,416 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -5,60 +5,76 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * SWEA_3307_최장증가부분수열
+ * @author parkrootseok
+ * 
+ * 1. 숫자의 갯수 입력
+ * 2. 수열 입력
+ * 3. 최장 증가 부분 수열 구하기
+ *  3-1. 현재 위치에 있는 수보다 비교할 위치에 있는 수가 더 클 때
+ *   3-1-1. 현재 위치에서 길이를 증가한 값이 비교할 위치에 있는 길이보다 길다면 갱신
+ **/
 class Solution {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static Scanner sc = new Scanner(System.in);
-
-    static int N, M, X, Y;
-    static int ANSWER;
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringBuilder sb;
+    static String[] inputs;
+    
+    static int testCaseNumber;
+    
+    static int number;
     static int[] sequence;
     static int[] dp;
 
-    static int[] dx = {0, 1, 1, 1};
-    static int[] dy = {1, 0, 1, -1};
-
-    public static void solution() {
-        
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (sequence[i] < sequence[j]) {
-                    dp[j] = Math.max(dp[j], dp[i] + 1);
-                }
-            }
-        }
-
-
-    }
-
-
     public static void main(String args[]) throws Exception {
 
-        int T = Integer.parseInt(br.readLine());
+    	br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+		
+        testCaseNumber = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= T; i++) {
+        for (int tc = 1; tc <= testCaseNumber; tc++) {
 
-            bw.write("#" + i + " ");
-            N = Integer.parseInt(br.readLine());
-            sequence = new int[N];
-            dp = new int[N];
-            Arrays.fill(dp, 1);
-            String[] inputs = br.readLine().split(" ");
-
-            int j = 0;
-            for (String input : inputs) {
-                sequence[j++] = Integer.parseInt(input);
+        	// 1. 숫자의 갯수 입력
+            number = Integer.parseInt(br.readLine());
+            
+            // 2. 수열을 입력받는다.
+            sequence = new int[number];
+            inputs = br.readLine().split(" ");
+            for (int idx = 0; idx < inputs.length; idx++) {
+                sequence[idx] = Integer.parseInt(inputs[idx]);
             }
 
-            solution();
+            // 3. 최장 증가 부분 수열 구하기
+            dp = new int[number];
+            Arrays.fill(dp, 1);
+            for (int curIdx = 0; curIdx < number - 1; curIdx++) {
+            	
+                for (int compareIdx = curIdx + 1; compareIdx < number; compareIdx++) {
+                	
+                	// 3-1. 현재 위치에 있는 수보다 비교할 위치에 있는 수가 더 클 때
+                    if (sequence[curIdx] < sequence[compareIdx]) {
+                    	
+                    	// 3-1-1. 현재 위치에서 길이를 증가한 값이 비교할 위치에 있는 길이보다 길다면 갱신 
+                        dp[compareIdx] = Math.max(dp[compareIdx], dp[curIdx] + 1);
+                    }
+                    
+                }
+                
+            }
+
             Arrays.sort(dp);
 
-            bw.write(dp[N - 1] + "\n");
+            sb.append("#").append(tc).append(" ").append(dp[number - 1]).append("\n");
 
         }
 
+        bw.write(sb.toString());
         bw.close();
+        return;
 
     }
 
