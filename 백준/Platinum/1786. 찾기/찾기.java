9dfe1e860c712e66cab0e81bd9e4394434a4d01d
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-03-29 16:31:38 +0900

    [Platinum V] Title: 찾기, Time: 3048 ms, Memory: 154964 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -1,121 +1,137 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Queue;
-
-import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
- * BOJ_1786_찾기
- * @author parkrootseok
+ * goal: pattern이 text에 등장하는 index를 출력
  * 
- * - KMP 알고리즘 
- *  
- * 1. 문자열과 찾을 패턴을 받는다.
- * 2. 패턴의 부분 문자열 중 접두사와 접미사가 일치하는 길이에 대한 정보를 생성
- * 3. 전처리한 정보를 통해 패턴이 일치하는 횟수와 나타나는 위치의 시작점 찾는다.
- **/
+ * 0. 정리할 데이터 및 함수
+ * 	0-1. ansCnt: 패턴이 몇번 등장하는지를 저장할 변수
+ * 	0-2. ansList: 패턴이 등장하는 인덱스를 저장할 int[]
+ * 	0-3. preprocessing(): 이동경로 테이블 moveTable을 생성하는 메소드
+ * 	0-4. textLength: 본문길이
+ * 	0-5. patternLength: 패턴길이
+ * 
+ * 
+ * 1. 입력
+ * 	1-1. text
+ * 	1-2. pattern
+ * 
+ * 2. KMP 알고리즘 구현
+ * 	2-1. 이동경로 테이블 생성
+ * 	2-2. 본문포인터 textPointer, 패턴포인터 patternPointer
+ * 	2-3. textPointer를 textLength길이까지 1씩 증가시키며 반복
+ * 		2-3-1. 본문과 패턴의 문자가 같으면 다음 문자 확인
+ * 		2-3-2. 본문과 패턴의 문자가 다르면 patternPointer를 이동경로테이블 기준으로 옮기기
+ * 		2-3-3. j가 patternLength까지 증가하면 ansCnt+1, ansList[ansCnt]=textPointer
+ * 
+ * 
+ * 
+ * 
+ */
+
 public class Main {
-	
 	static BufferedReader br;
-	static BufferedWriter bw;
 	static StringBuilder sb;
-	static String[] inputs;
-	static String input;
+	static StringTokenizer st;
 	
-	static String origin;
+	static int ansCnt;
+	static List<Integer> ansList;
+	
+	static String text;
 	static String pattern;
+	static int textLength;
+	static int patternLength;
+	static int textPointer=0;
+	static int patternPointer=0;
 	
-	static int[] table;
-	static int count;
+	static int[] moveTable;
+	
+	public static int[] preprocessing(int[] moveTable) {
+		// prefixPointer = 접두사 인덱스j, suffixPointer = 접미사 인덱스i
+	    int prefixPointer = 0;
+	    
+	    // 패턴을 처음부터 돌면서 체크 
+	    for (int suffixPointer = 1; suffixPointer < patternLength; suffixPointer++) { 
+	        // 만약 반복되는 패턴이 없으면, prefixPointer = 0 까지 돌아감 
+	        while(prefixPointer > 0 && pattern.charAt(suffixPointer) != pattern.charAt(prefixPointer)) {
+	        	prefixPointer = moveTable[prefixPointer-1];
+	        }
+	        
+	        // 일치할때, 접두사의 길이 저장 
+	        // 현재 맞은 idx의 +1은 길이이자, 다음 체크할 idx가 됨
+	        if (pattern.charAt(suffixPointer) == pattern.charAt(prefixPointer)) {
+	        	prefixPointer++;
+	        	//suffixPointer++;
+	        	moveTable[suffixPointer] = prefixPointer;
+	        }
+	    }
+	    
+	    return moveTable;
+	}
+	
+	
+	static void inputTestcase() throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		text = br.readLine();
+		pattern = br.readLine();
+		textLength = text.length();
+		patternLength = pattern.length();
+		ansCnt=0;
+		ansList = new ArrayList<Integer>();
+	}
 	
 	public static void main(String[] args) throws IOException {
-
-		br = new BufferedReader(new InputStreamReader(System.in));
-		bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		sb = new StringBuilder();
-
-		// 1. 문자열과 찾을 패턴을 받는다. 
-		origin = br.readLine();
-		pattern = br.readLine();
+		//입력
+		inputTestcase();
+//		System.out.println(text);
+//		System.out.println(pattern);
 		
-		// 2. 패턴의 부분 문자열 중 접두사와 접미사가 일치하는 길이에 대한 정보를 생성
-		init();
+		//KMP알고리즘 구현
+		//1. 이동경로 테이블 생성
+		moveTable = new int[patternLength];
+		moveTable = preprocessing(moveTable);
+//		for(int idx=0; idx<patternLength; idx++) {
+//			System.out.print(moveTable[idx]+" ");
+//		}
+//		System.out.println();
 		
-		// 3. 전처리한 정보를 통해 패턴이 일치하는 횟수와 나타나는 위치의 시작점 찾는다.
-		KMP();
 		
-		bw.write(count + "\n");
-		bw.write(sb.toString());
-		bw.close();
-		return;
-
-	}
-	
-	public static void init() {
-		
-		table = new int[pattern.length()];
-		
-		int prefixIdx = 0;
-		for (int suffixIdx = 1; suffixIdx < pattern.length(); suffixIdx++) {
-			
-			// 마지막 문자와 접두사의 첫 문자가 다를 경우
-			while(0 < prefixIdx && pattern.charAt(suffixIdx) != pattern.charAt(prefixIdx)) {
+		//2. 탐색
+		while(textPointer<textLength) {
+			//2-1. 본문과 패턴의 문자가 같으면 다음 문자 확인
+			//patternPointer==0 || 
+			if(text.charAt(textPointer)==pattern.charAt(patternPointer)) {
+				textPointer++;
+				patternPointer++;
+			}
+			else if(patternPointer==0) {
+				textPointer++;
+			}
+			//2-2. 본문과 패턴의 문자가 다르면 patternPointer를 이동경로테이블 기준으로 옮기기
+			else {
+				patternPointer=moveTable[patternPointer-1];
+			}
+			//2-3. patternPointer가 patternLength까지 증가하면 ansCnt+1, ansList[ansCnt]=textPointer
+			if(patternPointer==patternLength) {
+				ansCnt+=1;
+				ansList.add(textPointer-(patternLength-1));
 				
-				// 현재 문자열 길이보다 짧은 길이를 가지는 부븐 일치 문자열의 마지막으로 이동
-				prefixIdx = table[prefixIdx - 1];
+				patternPointer=moveTable[patternPointer-1];
+
+				
+				
 			}
-			
-			// 마지막 문자와 접두사의 첫 문자가 같을 경우 테이블에 부븐 일치 문자열 길이를 저장
-			if (pattern.charAt(suffixIdx) == pattern.charAt(prefixIdx)) {
-				table[suffixIdx] = ++prefixIdx;
-			}
-			
 		}
 		
-		return;
-		
-	} 
-	
-	public static void KMP() {
-		
-		count = 0;
-		int patternIdx = 0;
-		for (int originIdx = 0; originIdx < origin.length(); originIdx++) {
-			
-			// 본문에서의 마지막 문자와 접두사의 첫 문자가 다를 경우
-			while(0 < patternIdx && origin.charAt(originIdx) != pattern.charAt(patternIdx)) {
-				
-				// 현재 문자열 길이보다 짧은 구간의 마지막 문자열로 이동
-				patternIdx = table[patternIdx - 1];
-			}
-			
-			// 본문에서의 마지막 문자와 접두사의 첫 문자가 같을 경우
-			if (origin.charAt(originIdx) == pattern.charAt(patternIdx)) {
-				
-				// 패턴 문자열에서 마지막 이라면 카운팅 후 시작 위치를 기록
-				if (patternIdx == pattern.length() - 1) {
-					
-					count++;
-					sb.append((originIdx - patternIdx + 1) + " ");
-					
-					// 패턴 문자열의 마지막 문자를 부분 문자열로 가지는 부분 문자열의 시작점으로 이동 
-					patternIdx = table[patternIdx];
-					
-				}
-				
-				// 패턴 문자열에서 마지막이 아니라면 다음 문자로 이동
-				else {
-					patternIdx++;
-				}
-			
-			}
-			
+		//출력
+		System.out.println(ansCnt);
+		for(int idx=0; idx<ansList.size(); idx++) {
+			System.out.print(ansList.get(idx)+" ");
 		}
-		
 	}
-	
 }
