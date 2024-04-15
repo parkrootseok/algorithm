48c5985cf1e8574b3cf7b786253beef597485fad
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-04-15 23:58:24 +0900

    [Silver III] Title: 소수 구하기, Time: 200 ms, Memory: 18048 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -3,58 +3,68 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.Arrays;
 
 /**
- * 소수구하기
- */
-
+ * BOJ_1929_소수구하기
+ * @author parkrootseok
+ *
+ *  1. 소수를 판단할 숫자의 범위 입력 받는다.
+ *  2. 소수 체크 배열을 만든다.
+ *  3. 소수만 출력
+ **/
 public class Main {
 
-    public boolean solution(int num) {
+    static final int LIMIT = 1_000_000;
 
-        for (int i = 2 ; i < num ; i++) {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringBuilder sb;
+    static String[] inputs;
 
-            if (num % i == 0) {
-                return false;
-            }
-
-        }
-
-        return true;
-
-    }
+    static boolean[] isPrime;
+    static int start;
+    static int end;
 
     public static void main(String[] args) throws IOException {
 
-        Main m = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
 
-        int MN = 1000001;
-        boolean[] isPrime = new boolean[MN];
+        // 1. 소수를 판단할 숫자의 범위 입력 받는다.
+        inputs = br.readLine().trim().split(" ");
+        start = Integer.parseInt(inputs[0]);
+        end = Integer.parseInt(inputs[1]);
+
+        // 2. 소수 체크 배열을 만든다.
+        isPrime = new boolean[LIMIT + 1];
         Arrays.fill(isPrime, true);
 
         isPrime[0] = isPrime[1] = false;
-        for (int i = 2 ; i * i < MN ; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i ; j < MN ; j += i) {
-                    isPrime[j] = false;
+        for (int number = 2; number <= Math.sqrt(LIMIT); number++) {
+
+            // 2-1. 현재 수가 소수일 때 현재 수의 곱으로 만들 수 있는 수라면 소수가 아님
+            if (isPrime[number]) {
+
+                for (int checkNumber = number * number; checkNumber <= LIMIT; checkNumber += number) {
+                    isPrime[checkNumber] = false;
                 }
+
+            }
+
+        }
+
+        // 3. 소수만 출력
+        for (int number = start; number <= end; number++) {
+            if (isPrime[number]) {
+                sb.append(number).append("\n");
             }
         }
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int M = Integer.parseInt(st.nextToken()), N = Integer.parseInt(st.nextToken());
-
-
-        for (int i = M ; i <= N ; i++) {
-            if (isPrime[i]) {
-                bw.write(i + "\n");
-            }
-        }
-
+        bw.write(sb.toString());
         bw.close();
+        return;
 
     }
 
