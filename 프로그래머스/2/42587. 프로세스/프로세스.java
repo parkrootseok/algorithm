d98fa287d3d2f387d84eaa356f429726b623358e
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-07-27 22:08:40 +0900

    [level 2] Title: 프로세스, Time: 1.38 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,81 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * PG_프로세스
+ * @author parkrootseok
+ *
+ * - 프로세스는 다음과 같은 규칙에 따라 관리된다.
+ *  - 대기 큐에서 대기중인 프로세스를 하나 꺼낸다.
+ *  - 큐에 대기중인 프로세스 중 우선순위가 더 높은 프로세스가 있다면 다시 큐에 넣는다.
+ *  - 만약 없다면 꺼낸 프로세스를 실행한다.
+ * - 중요도 배열과 프로세스 위치 정보가 주어졌을 때 해당 위치에 있는 프로세스가 몇 번째로 실행되는지 찾아라.
+ * 
+ * 1. 대키큐에서 프로세스를 가져온다.
+ * 2. 본인보다 우선순위가 높은 큐가 있다면 다시 큐에 삽입한다.
+ * 3. 없다면 실행한다
+ */
+class Solution {
+    
+    public static class Process {
+
+		int index;
+		int priority;
+
+		public Process(int index, int priority) {
+			this.index = index;
+			this.priority = priority;
+		}
+
+	}
+    
+    public int solution(int[] priorities, int location) {
+
+		Queue<Process> waitingQ = new ArrayDeque<>();
+		for (int index = 0; index < priorities.length; index++) {
+			waitingQ.add(new Process(index, priorities[index]));
+		}
+
+		int order = 0;
+		boolean isPossible;
+		boolean[] isFinished = new boolean[priorities.length];
+		while (true) {
+
+			isPossible = true;
+
+			// 1. 대키큐에서 프로세스를 가져온다.
+			Process process = waitingQ.poll();
+
+			// 2. 본인보다 우선순위가 높은 큐가 있다면 다시 큐에 삽입한다.
+			for (int index = 0; index < priorities.length; index++) {
+
+				if (isFinished[index]) {
+					continue;
+				}
+
+				if (process.priority < priorities[index]) {
+					waitingQ.offer(process);
+					isPossible = false;
+					break;
+				}
+
+			}
+
+			// 3. 없다면 실행한다
+			if (!isPossible) {
+				continue;
+			}
+
+			isFinished[process.index] = true;
+			order++;
+
+			if (process.index == location) {
+				break;
+			}
+
+		}
+
+		return order;
+
+	}
+}
