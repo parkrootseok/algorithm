1c7c757ba316a2ae8cc0def81bcfa924516299ba
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-08-01 22:22:53 +0900

    [level 3] Title: 단어 변환, Time: 0.79 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+/**
+ * PG_단어 변환
+ * @author parkrootseok
+ *
+ * - begin에서 target으로 변환하는 가장 짧은 과정을 찾아라
+ *   - 단, 한 번에 한 개의 알파벳만 바꿀 수 있으며, 주어진 단어 안에서만 변환이 가능
+ *
+ * 1. 변환할 수 있는 모든 단어에 대하여 큐에 삽입
+ *  1-1. 현재 단어와 바꿀 단어와 똑같은 알파벳의 개수를 구하고
+ *  1-2. 똑같은 알파벳의 갯수가 총 2개라면 변환 가능
+ */
+public class Solution {
+
+	public static class Node {
+
+		String string;
+		int depth;
+
+		public Node(String string, int depth) {
+			this.string = string;
+			this.depth = depth;
+		}
+	}
+
+	public static Set<String> isVisited;
+    
+	public int solution(String begin, String target, String[] words) {
+
+		boolean isPossible = false;
+		for (String word : words) {
+
+			if (word.equals(target)) {
+				isPossible = true;
+			}
+
+		}
+
+		if (!isPossible) {
+			return 0;
+		}
+
+		isVisited = new HashSet<>();
+
+		return bfs(begin, target, words);
+
+	}
+
+	public int bfs(String begin, String target, String[] words) {
+
+		Queue<Node> nodeQ = new ArrayDeque<>();
+		nodeQ.add(new Node(begin, 0));
+		isVisited.add(begin);
+
+		while (!nodeQ.isEmpty()) {
+
+			Node node = nodeQ.poll();
+			String curString = node.string;
+			int depth = node.depth;
+
+			if (curString.equals(target)) {
+				return depth;
+			}
+
+			// 1. 변환할 수 있는 모든 단어에 대하여 큐에 삽입
+			for (int index = 0; index < words.length; index++) {
+
+				if (isVisited.contains(words[index])) {
+					continue;
+				}
+
+				// 1-1. 현재 단어와 바꿀 단어와 똑같은 알파벳의 개수를 구하고
+				int number = 0;
+				for (int pos = 0; pos < words[index].length(); pos++) {
+
+					if (curString.charAt(pos) != words[index].charAt(pos)) {
+						number++;
+					}
+
+				}
+
+				// 1-2. 똑같은 알파벳의 갯수가 총 2개라면 변환 가능
+				if (number == 1) {
+					isVisited.add(words[index]);
+					nodeQ.offer(new Node(words[index], depth + 1));
+				}
+
+			}
+
+		}
+
+		return 0;
+
+	}
+
+}
