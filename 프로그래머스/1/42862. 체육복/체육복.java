6fe44681a9c23c5c2fe420aeba6ea4d586b1aa27
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-08-04 00:37:38 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -15,29 +15,29 @@ public class Solution {
 	public int solution(int n, int[] lost, int[] reserve) {
 
 		int ans = n;
-		int[] isPossible = new int[n];
+		int[] isPossible = new int[n + 2];
 
 		// 1. 잃어버린 학생을 체크
 		for (int l : lost) {
-			isPossible[l - 1]--;
+			isPossible[l]--;
 		}
 
 		// 2. 여벌옷이 있는 사람 체크
 		for (int r : reserve) {
-			isPossible[r - 1]++;
+			isPossible[r]++;
 		}
 
 		// 3. 여벌옷도 없고 잃어버린 사람들 체크
-		for (int index = 0; index < isPossible.length; index++) {
+		for (int index = 1; index <= n; index++) {
 
 			// 3-1. 체육복울 잃어버린 사람이라면 자신의 앞뒤에 빌릴 수 있는지 확인
 			if (isPossible[index] == -1) {
 
-				if (index - 1 >= 0 && isPossible[index - 1] == 1) {
+				if (isPossible[index - 1] == 1) {
 					isPossible[index]++;
 					isPossible[index-1]--;
 				}
-				else if (index + 1 < isPossible.length && isPossible[index + 1] == 1) {
+				else if (isPossible[index + 1] == 1) {
 					isPossible[index]--;
 					isPossible[index+1]--;
 				} else {
