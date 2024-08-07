b2220258cc2d253f686571a96d621abcca2aba83
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-08-07 19:43:31 +0900

    [level 2] Title: 카펫, Time: 8.23 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 93.6 MB, 시간: 19.20 ms
+메모리: 74.1 MB, 시간: 8.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 07일 19:33:28
+2024년 08월 07일 19:43:22
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -11,51 +11,31 @@ import java.util.*;
 public class Solution {
 
 	public static int[] answer;
-	public static boolean[][] isVisited;
 	public boolean isFinished;
 
 	public int[] solution(int brown, int yellow) {
 
 		answer = new int[2];
-		isVisited = new boolean[2450][2450];
 		isFinished = false;
-		bruteforce(brown, yellow, 3, 3);
-		return answer;
 
-	}
+		for (int col = 3; ;col++) {
 
-	public void bruteforce(int brown, int yellow, int col, int row) {
+			for (int row = 3; row <= col; row++) {
 
-		int targetBrown = (col * 2) + (row - 2) * 2;
-		int targetYellow = (row * col) - targetBrown;
+				int targetB = (col * 2) + (row - 2) * 2;
+				int targetY = col * row - targetB;
 
-		if (isFinished) {
-			return;
-		}
+				if (targetB == brown && targetY == yellow) {
+					answer[0] = col;
+					answer[1] = row;
+					return answer;
+				}
 
-		if (isVisited[col][row]) {
-			return;
-		}
+			}
 
-		if (targetBrown > brown || targetYellow > yellow) {
-			return;
-		}
-
-		if (targetBrown == brown && targetYellow == yellow) {
-			answer[0] = col;
-			answer[1] = row;
-			isFinished = true;
-			return;
-		}
-
-		isVisited[col][row] = true;
-
-		bruteforce(brown, yellow, col + 1, row);
-
-		if (col >= row + 1) {
-			bruteforce(brown, yellow, col, row + 1);
 		}
 
 	}
+
 
 }
