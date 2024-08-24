6796f070560d99e28ecc5ee5788c6718d4944ce6
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-08-24 16:39:06 +0900

    [Silver II] Title: N과 M (10), Time: 68 ms, Memory: 11720 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.TreeSet;
+
+/**
+ * BOJ_N과M
+ * @author parkrootseok
+ */
+public class Main {
+
+	public static BufferedReader br;
+	public static BufferedWriter bw;
+	public static StringBuilder sb;
+	public static String[] inputs;
+
+	public static int N;
+	public static int M;
+	public static int[] numbers;
+	public static int[] sequence;
+	public static TreeSet<String> isUsed;
+	public static boolean[] isVisited;
+
+	public static void main(String[] args) throws IOException {
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+
+		inputs = br.readLine().trim().split(" ");
+		N = Integer.parseInt(inputs[0]);
+		M = Integer.parseInt(inputs[1]);
+
+		numbers = new int[N];
+
+		inputs = br.readLine().trim().split(" ");
+		for (int idx = 0; idx < N; idx++) {
+			numbers[idx] = Integer.parseInt(inputs[idx]);
+		}
+
+		Arrays.sort(numbers);
+		isUsed = new TreeSet<>();
+		sequence = new int[M];
+		isVisited = new boolean[N];
+		sequence(0, 0);
+
+		bw.write(sb.toString());
+		bw.close();
+
+	}
+
+	public static void sequence(int depth, int start) {
+
+		if (depth == M) {
+
+			StringBuilder sb = new StringBuilder();
+
+			for (int idx = 0; idx < M; idx++) {
+				sb.append(sequence[idx]).append(" ");
+			}
+
+			if (isUsed.contains(sb.toString())) {
+				return;
+			}
+
+			isUsed.add(sb.toString());
+			System.out.println(sb);
+
+			return;
+
+		}
+
+		for (int idx = start; idx < N; idx++) {
+
+			if (isVisited[idx]) {
+				continue;
+			}
+
+			isVisited[idx] = true;
+			sequence[depth] = numbers[idx];
+			sequence(depth + 1, idx + 1);
+			isVisited[idx] = false;
+
+		}
+
+	}
+
+}
