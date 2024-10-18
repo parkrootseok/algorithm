7f501567a50d86e7709a844d6219a17931e55531
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-10-18 22:24:02 +0900

    [Gold V] Title: 애너그램, Time: 344 ms, Memory: 30092 KB -BaekjoonHub

--- 백준/Gold/6443. 애너그램/애너그램.java
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -1,49 +1,66 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
+/**
+ * BOJ_에너그램
+ * @author parkrootseok
+ */
 public class Main {
 
-	static char[] arr;
-	static int[] check;
-	static StringBuilder sb;
-	static Stack<Character> s;
-	static Set<String> set;
+	public static BufferedReader br;
+	public static BufferedWriter bw;
+	public static StringBuilder sb;
+
+	static int N;
+	static char[] letters;
+	static boolean[] isUsed;
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringBuilder result = new StringBuilder();
-		int n = Integer.parseInt(br.readLine());
-		while(n-- > 0) {
-			set = new TreeSet<>();
-			arr = br.readLine().toCharArray();
-			check = new int[26];
-			for(char c : arr) {
-				check[c-'a']++;
-			}
-			s = new Stack<>();
-			comb(arr.length);
-			set.stream().forEach(s -> result.append(s).append("\n"));
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+
+		N = Integer.parseInt(br.readLine().trim());
+		for (int count = 0; count < N; count++) {
+
+			letters = br.readLine().trim().toCharArray();
+			isUsed = new boolean[letters.length];
+
+			Arrays.sort(letters);
+			permutation(new StringBuilder(), 0);
+
 		}
-		System.out.println(result.toString());
+
+		bw.close();
+
 	}
-	
-	static void comb(int r) {
-		if(r == s.size()) {
-			StringBuilder sb = new StringBuilder();
-			for(char c : s) {
-				sb.append(c);
-			}
-			set.add(sb.toString());
+
+	public static void permutation(StringBuilder cur, int depth) throws IOException {
+
+		if (depth == letters.length) {
+
+			bw.write(cur.toString());
+			bw.write('\n');
+			return;
+
 		}
-		
-		for(int i=0; i<26; i++){
-			if(check[i] > 0) {
-				check[i]--;
-				s.push((char)(i+'a'));
-				comb(r);
-				s.pop();
-				check[i]++;
+
+		for (int index = 0; index < letters.length; index++) {
+
+			// 만약, 이전 문자와 동일하다면 이전 문자가 사용되지 않은 경우 패스
+			if (isUsed[index] || (0 < index && letters[index] == letters[index - 1] && !isUsed[index - 1])) {
+				continue;
 			}
+
+			isUsed[index] = true;
+			cur.append(letters[index]);
+			permutation(cur, depth + 1);
+			cur.deleteCharAt(cur.length() - 1);
+			isUsed[index] = false;
+
 		}
+
 	}
+
 }
