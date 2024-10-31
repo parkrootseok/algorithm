ce5adc4a8de6972efd35e366397e7b8e1557a469
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-10-31 18:20:44 +0900

    [Gold IV] Title: 플로이드, Time: 448 ms, Memory: 58104 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.Arrays;
 
 /**
  * BOJ_플로이드
@@ -11,41 +8,12 @@ import java.util.Queue;
 
 public class Main {
 
-    static class City {
-
-        int name;
-        List<Node> adjacentCities = new ArrayList<>();
-
-        public City(int name) {
-            this.name = name;
-        }
-
-    }
-
-    static class Node implements Comparable<Node> {
-
-        int name;
-        int cost;
-
-        public Node(int name, int cost) {
-            this.name = name;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Node n) {
-            return Integer.compare(this.cost, n.cost);
-        }
-
-    }
-
     public static BufferedReader br;
     public static BufferedWriter bw;
     public static StringBuilder sb;
 
     static final int INF = 100_000_000;
 
-    static City[] cities;
     static int cityCount;
     static int busCount;
     static int[][] cost;
@@ -57,9 +25,9 @@ public class Main {
         sb = new StringBuilder();
 
         cityCount = Integer.parseInt(br.readLine().trim());
-        cities = new City[cityCount];
+        cost = new int[cityCount][cityCount];
         for (int city = 0; city < cityCount; city++) {
-            cities[city] = new City(city);
+            Arrays.fill(cost[city], INF);
         }
 
         busCount = Integer.parseInt(br.readLine().trim());
@@ -70,20 +38,26 @@ public class Main {
             int to = Integer.parseInt(inputs[1]);
             int c = Integer.parseInt(inputs[2]);
 
-            cities[from - 1].adjacentCities.add(new Node(to - 1, c));
+            cost[from - 1][to - 1] = Math.min(cost[from - 1][to - 1], c);
         }
 
-        cost = new int[cityCount][cityCount];
-        for (int city = 0; city < cityCount; city++) {
-            dijkstra(city);
+        for (int s = 0; s < cityCount; s++) {
+            for (int from = 0; from < cityCount; from++) {
+                for (int to = 0; to < cityCount; to++) {
+                    if (from != to && cost[from][to] > cost[from][s] + cost[s][to]) {
+                        cost[from][to] = cost[from][s] + cost[s][to];
+                    }
+
+                }
+            }
         }
 
         for (int from = 0; from < cityCount; from++) {
             for (int to = 0; to < cityCount; to++) {
                 if (cost[from][to] == INF) {
-                    sb.append("0").append(" ");    
+                    sb.append("0").append(" ");
                 } else {
-                    sb.append(cost[from][to]).append(" ");    
+                    sb.append(cost[from][to]).append(" ");
                 }
             }
             sb.append("\n");
@@ -91,40 +65,6 @@ public class Main {
 
         bw.write(sb.toString());
         bw.close();
-
-    }
-
-    public static void dijkstra(int start) {
-
-        for (int city = 0; city < cityCount; city++) {
-            cost[start][city] = INF;
-        }
-
-        boolean[] isVisited = new boolean[cityCount];
-        PriorityQueue<Node> nodes = new PriorityQueue<>();
-        nodes.add(new Node(start, 0));
-        cost[start][start] = 0;
-
-        while (!nodes.isEmpty()) {
-
-            Node cNode = nodes.poll();
-
-            if (isVisited[cNode.name]) {
-                continue;
-            }
-
-            isVisited[cNode.name] = true;
-
-            for (Node nNode : cities[cNode.name].adjacentCities) {
-
-                if (cost[start][nNode.name] > cost[start][cNode.name] + nNode.cost) {
-                    cost[start][nNode.name] = cost[start][cNode.name] + nNode.cost;
-                    nodes.add(new Node(nNode.name, cost[start][nNode.name]));
-                }
-
-            }
-
-        }
 
     }
 
