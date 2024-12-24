3edbfa2c884978faab6cb6168db2a78a36d92363
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-12-24 17:33:09 +0900

    [Silver II] Title: 나무 자르기, Time: 456 ms, Memory: 167920 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,24 +1,21 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.StringTokenizer;
 
 /**
  * BOJ_나무자르기
  * @author parkrootseok
  */
-public class Main {
+class Main {
 
-	public static BufferedReader br;
-	public static BufferedWriter bw;
-	public static StringBuilder sb;
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringBuilder sb;
+	static StringTokenizer st;
 
-	public static int N;
-	public static int M;
-	public static int[] heights;
+	static int treeCount;
+	static int max;
+	static int goal;
+	static int[] treeHeights;
 
 	public static void main(String[] args) throws IOException {
 
@@ -26,53 +23,60 @@ public class Main {
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		sb = new StringBuilder();
 
-		String[] inputs = br.readLine().trim().split(" ");
-		N = Integer.parseInt(inputs[0]);
-		M = Integer.parseInt(inputs[1]);
+		input();
 
-		inputs = br.readLine().trim().split(" ");
-
-		int max = Integer.MIN_VALUE;
-		heights = new int[N];
-		for (int index = 0; index < N; index++) {
-			heights[index] = Integer.parseInt(inputs[index]);
-			max = Math.max(max, heights[index]);
-		}
-
-		sb.append(upper(0, max));
+		sb.append(binarySearch(0, max));
 		bw.write(sb.toString());
 		bw.close();
 
 	}
 
-	public static int upper(int low, int high) {
+	public static int binarySearch(int left, int right) {
 
-		while (low < high) {
+		while (left < right) {
 
-			int mid = (low + high) / 2;
-			long height = getHeight(mid);
+			int sawHeight = (left + right) >> 1;
+			long hasTreeLength = cut(sawHeight);
 
-			// 현재 가져갈 수 있는 나무가 M 이하 (더 많은 나무를 가져가야 하므로 높이를 줄여야 함)
-			if (height < M) {
-				high = mid;
+			if (hasTreeLength < goal) {
+				right = sawHeight;
 			} else {
-				low = mid + 1;
+				left = sawHeight + 1;
 			}
 
 		}
 
-		return high - 1;
+		return right - 1;
 
 	}
 
-	public static long getHeight(int mid) {
+	public static long cut(int mid) {
 
-		long height = 0;
-		for (int idx = 0; idx < N; idx++) {
-			height += (heights[idx] - mid > 0 ? heights[idx] - mid : 0);
+		long totalLength = 0;
+
+		for (int treeHeight : treeHeights) {
+			if (mid < treeHeight) {
+				totalLength += (treeHeight - mid);
+			}
 		}
 
-		return height;
+		return totalLength;
+
+	}
+
+	public static void input() throws IOException {
+
+		st = new StringTokenizer(br.readLine(), " ");
+		treeCount = Integer.parseInt(st.nextToken());
+		goal = Integer.parseInt(st.nextToken());
+
+		max = Integer.MIN_VALUE;
+		st = new StringTokenizer(br.readLine(), " ");
+		treeHeights = new int[treeCount];
+		for (int index = 0; index < treeCount; index++) {
+			treeHeights[index] = Integer.parseInt(st.nextToken());
+			max = Math.max(max, treeHeights[index]);
+		}
 
 	}
 
