451e437ed65d0543373f1e84b473bb764d1fe400
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-12-28 22:24:37 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,14 +1,14 @@
-SELECT 
+SELECT
     ID, 
     (CASE
-        WHEN r.rank <= 0.25 THEN 'CRITICAL'
-        WHEN r.rank <= 0.5 THEN 'HIGH'
-        WHEN r.rank <= 0.75 THEN 'MEDIUM'
+        WHEN per <= 0.25 THEN 'CRITICAL'
+        WHEN per <= 0.5 THEN 'HIGH'
+        WHEN per <= 0.75 THEN 'MEDIUM'
         ELSE 'LOW'
-    END) AS COLONY_NAME
-
+    END) COLONY_NAME
+    
 FROM
-    (SELECT ID, PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) AS `rank` FROM ECOLI_DATA) AS r
+    (SELECT ID, PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) AS per FROM ECOLI_DATA) AS r
     
 ORDER BY
     1
