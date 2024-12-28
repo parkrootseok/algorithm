ddf3abf576da52004621a346be41f49c289185a3
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-12-28 18:28:48 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,25 +1,30 @@
-WITH EVALUATION as (
-    SELECT  EMP_NO, 
-            (CASE 
-                WHEN 96 <= AVG(SCORE) THEN 'S'
-                WHEN 90 <= AVG(SCORE) THEN 'A'
-                WHEN 80 <= AVG(SCORE) THEN 'B'
-                ELSE 'C'
-            END) as GRADE
-    FROM    HR_GRADE
-    GROUP BY EMP_NO, YEAR
+WITH GRADES AS (
+    SELECT
+        EMP_NO,
+        (CASE
+            WHEN 96 <= AVG(SCORE) THEN 'S'   
+            WHEN 90 <= AVG(SCORE) THEN 'A'   
+            WHEN 80 <= AVG(SCORE) THEN 'B'  
+            ELSE 'C'    
+        END) AS GRADE
+    
+    FROM
+        HR_GRADE
+    
+    GROUP BY
+        EMP_NO
+    
 )
 
-SELECT  he.EMP_NO,
-        he.EMP_NAME,
-        GRADE,
-        (CASE GRADE
-                WHEN 'S' THEN SAL * 0.2
-                WHEN 'A' THEN SAL * 0.15
-                WHEN 'B' THEN SAL * 0.1
-                ELSE 0
-        END) as BONUS
-
-FROM    HR_EMPLOYEES he LEFT JOIN EVALUATION e ON he.EMP_NO = e.EMP_NO
-
-ORDER BY EMP_NO
+SELECT 
+    e.EMP_NO, e.EMP_NAME, GRADE, 
+    CASE GRADE
+        WHEN 'S' THEN SAL * 0.2
+        WHEN 'A' THEN SAL * 0.15
+        WHEN 'B' THEN SAL * 0.1
+        ELSE 0
+    END AS BONUS
+    
+FROM 
+    HR_EMPLOYEES e
+    JOIN GRADES g ON e.EMP_NO = g.EMP_NO
