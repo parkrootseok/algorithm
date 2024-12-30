043f3b4028a51d67668ecb9357440db7cef20486
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-12-30 17:34:28 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,28 +1,34 @@
-WITH 
-    FE AS (
-        SELECT BIT_OR(CODE) AS SKILL_CODE
-        FROM SKILLCODES
-        WHERE CATEGORY = 'Front End'
-    ),
-    GRADE AS (
-        SELECT 
-            CASE
-                WHEN 
-                    dev.SKILL_CODE & FE.SKILL_CODE 
-                    AND dev.SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME='Python')
-                THEN 'A'
-                WHEN 
-                    dev.SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME='C#') 
-                THEN 'B'
-                WHEN dev.SKILL_CODE & FE.SKILL_CODE THEN 'C'
-            END AS GRADE, ID, EMAIL
-        FROM DEVELOPERS dev, FE
-    )
-    
-SELECT GRADE, ID, EMAIL
+WITH F AS (
+    SELECT SUM(CODE) CODE FROM SKILLCODES WHERE CATEGORY='Front End'
+), GRADES AS (
+    SELECT
+    CASE
+        WHEN 
+            f.CODE & d.SKILL_CODE 
+            AND (SELECT CODE FROM SKILLCODES WHERE NAME='Python') & d.SKILL_CODE 
+            THEN 'A'
+        
+        WHEN (SELECT CODE FROM SKILLCODES WHERE NAME='C#') & d.SKILL_CODE 
+            THEN 'B'
+        
+        WHEN f.CODE & d.SKILL_CODE 
+            THEN 'C'
+            
+    END GRADE, 
+    ID, EMAIL
 
-FROM GRADE
-    
-WHERE GRADE IS NOT NULL
+    FROM
+        DEVELOPERS d, F f
+)
 
-ORDER BY GRADE, ID
+SELECT
+    GRADE, ID, EMAIL
+    
+FROM
+    GRADES
+
+WHERE
+    GRADE IS NOT NULL
+
+ORDER BY
+    1, 2
