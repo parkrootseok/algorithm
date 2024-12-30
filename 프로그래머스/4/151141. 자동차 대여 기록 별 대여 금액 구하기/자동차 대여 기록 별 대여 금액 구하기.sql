d0b07349a2a02c2ff27fd63eeb0623c205aa9fa9
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-12-30 18:32:16 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,31 +1,33 @@
 WITH DISCOUNT AS (
     
-    SELECT
-            ccrh.HISTORY_ID, 
-            ccr.CAR_TYPE, 
-            ccr.DAILY_FEE,
-            DATEDIFF(ccrh.END_DATE, ccrh.START_DATE) + 1 AS period,
-            (CASE 
-                WHEN 90 <= (DATEDIFF(ccrh.END_DATE, ccrh.START_DATE) + 1) THEN '90일 이상'   
-                WHEN 30 <= (DATEDIFF(ccrh.END_DATE, ccrh.START_DATE) + 1) THEN '30일 이상'   
-                WHEN 7 <= (DATEDIFF(ccrh.END_DATE, ccrh.START_DATE) + 1)  THEN '7일 이상'
-                ELSE 'NONE'
-             END) AS DURATION_TYPE
+    SELECT 
+        history_id, c.CAR_TYPE, c.DAILY_FEE * (DATEDIFF(end_date, start_date) + 1) FEE,
+        CASE
+            WHEN 90 <= (DATEDIFF(end_date, start_date) + 1) THEN '90일 이상'
+            WHEN 30 <= (DATEDIFF(end_date, start_date) + 1) THEN '30일 이상'
+            WHEN 7 <= (DATEDIFF(end_date, start_date) + 1) THEN '7일 이상'
+            ELSE '7일 이하'
+        END DURATION_TYPE
     
     FROM
-        CAR_RENTAL_COMPANY_RENTAL_HISTORY ccrh
-            JOIN CAR_RENTAL_COMPANY_CAR ccr ON ccrh.CAR_ID = ccr.CAR_ID
+        CAR_RENTAL_COMPANY_RENTAL_HISTORY h
+            JOIN CAR_RENTAL_COMPANY_CAR c ON h.CAR_ID = c.CAR_ID
+            
     
-    WHERE ccr.CAR_TYPE = '트럭'
-
+    WHERE
+        c.CAR_TYPE = '트럭'
+    
 )
 
+
 SELECT
-    d.HISTORY_ID,
-    ROUND(d.DAILY_FEE * d.period * (100 - IFNULL(ccdp.DISCOUNT_RATE, 0)) / 100) AS FEE
+    HISTORY_ID, ROUND(FEE * (100 - IFNULL(DISCOUNT_RATE, 0)) / 100, 0) FEE
 
-FROM DISCOUNT d
-        LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN ccdp 
-        ON d.DURATION_TYPE = ccdp.DURATION_TYPE AND d.CAR_TYPE = ccdp.CAR_TYPE
-
-ORDER BY FEE DESC, HISTORY_ID DESC
+FROM
+    DISCOUNT d
+        LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN p
+        ON d.DURATION_TYPE = p.DURATION_TYPE AND d.CAR_TYPE = p.CAR_TYPE
+ 
+ORDER BY
+    2 DESC, 1 DESC
+        
