7df814f33222f6664be09266be87ada6d6529281
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-03-18 09:39:50 +0900

    [Gold IV] Title: 전화번호 목록, Time: 472 ms, Memory: 245148 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -3,9 +3,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.StreamTokenizer;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.StringTokenizer;
 
 /**
  * BOJ_전화번호목록
@@ -13,24 +14,40 @@ import java.util.Map;
  */
 public class Main {
 
+	static class Trie {
+		Node root;
+
+		public Trie() {
+			this.root = new Node();
+		}
+	}
+
 	static class Node {
 
-		Map<Character, Node> children = new HashMap<>();
-		boolean isEnd;
+		Map<Character, Node> children;
+		boolean isLast;
+
+		public Node() {
+			this.children = new HashMap<>();
+			this.isLast = false;
+		}
 
 		public void insert(String word) {
 
 			Node node = this;
-			for (int idx = 0; idx < word.length(); idx++) {
 
-				char c = word.charAt(idx);
+			for (int index = 0; index < word.length(); index++) {
 
+				char c = word.charAt(index);
+
+				// 현재 Node에 c를 가지는 노드가 없다면, Node를 할당
 				node.children.putIfAbsent(c, new Node());
+
+				// 다음 c를 추가할 노드 가져오기
 				node = node.children.get(c);
 
-				if (idx == word.length() - 1) {
-					node.isEnd = true;
-					return;
+				if (index == word.length() - 1) {
+					node.isLast = true;
 				}
 
 			}
@@ -40,24 +57,28 @@ public class Main {
 		public boolean contains(String word) {
 
 			Node node = this;
-			for (int idx = 0; idx < word.length(); idx++) {
 
-				char c = word.charAt(idx);
+			for (char c : word.toCharArray()) {
+
+				// c를 가지는 노드를 가져온다.
 				Node child = node.children.get(c);
 
-				if (child == null) {
+				// 없다면, 포함하지 않는 word
+				if (Objects.isNull(child)) {
 					return false;
 				}
 
+				// 있다면, 다음 자식을 조사
 				node = child;
 
 			}
 
-			if (node.isEnd && node.children.isEmpty()) {
-				return false;
+			// 마지막으로 검사한 노드가 어떤 한 문장의 끝인데, 자식이 없다면
+			if (node.isLast && !node.children.isEmpty()) {
+				return true;
 			}
 
-			return true;
+			return false;
 
 		}
 
@@ -65,12 +86,11 @@ public class Main {
 
 	static BufferedReader br;
 	static BufferedWriter bw;
-	static StreamTokenizer st;
-	static StringBuilder sb;
+	static StringTokenizer st;
+	static StringBuilder sb;;
 
-	static int testcaseNumber;
-	static int telNumber;
-	static Node trie;
+	static int T;
+	static int N;
 
 	public static void main(String[] args) throws IOException {
 
@@ -78,27 +98,27 @@ public class Main {
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		sb = new StringBuilder();
 
-		testcaseNumber = Integer.parseInt(br.readLine().trim());
-		for (int tc = 0; tc < testcaseNumber; tc++) {
+		T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
 
-			boolean isPossible = true;
-			trie = new Node();
-			telNumber = Integer.parseInt(br.readLine().trim());
+			N = Integer.parseInt(br.readLine());
 
-			String[] words = new String[telNumber];
-			for (int tIdx = 0; tIdx < telNumber; tIdx++) {
-				words[tIdx] = br.readLine();
-				trie.insert(words[tIdx]);
+			Trie trie = new Trie();
+			String[] words = new String[N];
+			for (int n = 0; n < N; n++) {
+				words[n] = br.readLine();
+				trie.root.insert(words[n]);
 			}
 
+			boolean flag = true;
 			for (String word : words) {
-				if (trie.contains(word)) {
-					isPossible = false;
+				if (trie.root.contains(word)) {
+					flag = false;
 					break;
 				}
 			}
 
-			sb.append(isPossible ? "YES" : "NO").append("\n");
+			sb.append(flag? "YES\n" : "NO\n");
 
 		}
 
