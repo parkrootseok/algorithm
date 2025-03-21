a785db5bd5b433c7b348689eebd8c6ff32a784bb
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-03-21 18:56:50 +0900

    [Gold II] Title: 대표 선수, Time: 1272 ms, Memory: 177320 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.java
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -3,9 +3,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
@@ -14,15 +13,33 @@ import java.util.StringTokenizer;
  */
 public class Main {
 
+	static class Student implements Comparable<Student> {
+
+		int classNumber;
+		int ability;
+
+		public Student(int classNumber, int ability) {
+			this.classNumber = classNumber;
+			this.ability = ability;
+		}
+
+		@Override
+		public int compareTo(Student s) {
+			return this.ability - s.ability;
+		}
+
+	}
+
 	static BufferedReader br;
 	static BufferedWriter bw;
 	static StringTokenizer st;
-	static StringBuilder sb;;
+	static StringBuilder sb;
 
-	static int classNumber;
-	static int studentNumber;
-	static int[][] abilities;
-	static int[] students;
+	static int N;
+	static int M;
+	static Queue<Integer>[] abilities;
+	static Queue<Student> candidates;
+	static int max, min;
 
 	public static void main(String[] args) throws IOException {
 
@@ -31,62 +48,63 @@ public class Main {
 		sb = new StringBuilder();
 
 		st = new StringTokenizer(br.readLine(), " ");
-		classNumber = Integer.parseInt(st.nextToken());
-		studentNumber = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-		abilities = new int[classNumber][studentNumber];
-		for (int row = 0; row < classNumber; row++) {
+		min = Integer.MAX_VALUE;
+		max = Integer.MIN_VALUE;
+
+		abilities = new PriorityQueue[N];
+		candidates = new PriorityQueue<>();
+		for (int classNum = 0; classNum < N; classNum++) {
+
+			abilities[classNum] = new PriorityQueue<>();
 			st = new StringTokenizer(br.readLine(), " ");
-			for (int col = 0; col < studentNumber; col++) {
-				abilities[row][col] = Integer.parseInt(st.nextToken());
+			for (int student = 0; student < M; student++) {
+				abilities[classNum].add(Integer.parseInt(st.nextToken()));
 			}
-			Arrays.sort(abilities[row]);
+
+			int ability = abilities[classNum].poll();
+			candidates.add(new Student(classNum, ability));
+			min = Math.min(min, ability);
+			max = Math.max(max, ability);
+
 		}
 
-		sb.append(twoPointer());
+		sb.append(solution());
 		bw.write(sb.toString());
 		bw.close();
 
 	}
 
-	public static int twoPointer() {
+	public static int solution() {
 
-		students = new int[classNumber];
-		Arrays.fill(students, studentNumber - 1);
-
-		int result = Integer.MAX_VALUE;
+		int diff = max - min;
 		while (true) {
 
-			int max = Integer.MIN_VALUE;
-			int maxPos = 0;
-			int min = Integer.MAX_VALUE;
-
-			for (int c = 0; c < classNumber; c++) {
-
-				int ability = abilities[c][students[c]];
-
-				if (max < ability) {
-					max = ability;
-					maxPos = c;
-				}
-
-				if (min > ability) {
-					min = ability;
-				}
-
-			}
-
-			result = Math.min(result, max - min);
-
-			if (--students[maxPos] == -1) {
+			// 후보 중 최소 능력치를 가진 학생 추출
+			Student student = candidates.poll();
+			if (abilities[student.classNumber].isEmpty()) {
 				break;
 			}
 
+			// 후보 중 가장 최솟값을 가지는 학생이 속한 반에서 다음 후보가 될 학생을 삽입
+			int ability = abilities[student.classNumber].poll();
+			candidates.offer(new Student(student.classNumber, ability));
+
+			// 후보중 가장 낮은 능력치
+			min = candidates.peek().ability;
+
+			// 다음 후보가 될 학생의 능력치가 가장 큰지 확인
+			max = Math.max(max, ability);
+
+			// 가장 높은 능력치와 가장 낮은 능력치의 차가 더 작다면 갱신
+			diff = Math.min(diff, max - min);
+
 		}
 
-		return result;
+		return diff;
 
 	}
-
 
 }
