87ade3e2a82fde456b6a26f3ef9650defcec3d73
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-03-24 15:30:06 +0900

    [Gold II] Title: 대표 선수, Time: 1176 ms, Memory: 175220 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.java
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -14,7 +15,6 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static class Student implements Comparable<Student> {
-
 		int classNumber;
 		int ability;
 
@@ -25,9 +25,8 @@ public class Main {
 
 		@Override
 		public int compareTo(Student s) {
-			return this.ability - s.ability;
+			return Integer.compare(this.ability, s.ability);
 		}
-
 	}
 
 	static BufferedReader br;
@@ -51,24 +50,24 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		min = Integer.MAX_VALUE;
 		max = Integer.MIN_VALUE;
-
+		min = Integer.MAX_VALUE;
 		abilities = new PriorityQueue[N];
 		candidates = new PriorityQueue<>();
-		
-		for (int classNum = 0; classNum < N; classNum++) {
 
-			abilities[classNum] = new PriorityQueue<>();
+		for (int n = 0; n < N; n++) {
+
+			abilities[n] = new PriorityQueue<>();
 			st = new StringTokenizer(br.readLine(), " ");
-			for (int student = 0; student < M; student++) {
-				abilities[classNum].add(Integer.parseInt(st.nextToken()));
+
+			for (int m = 0; m < M; m++) {
+				abilities[n].offer(Integer.parseInt(st.nextToken()));
 			}
 
-			int ability = abilities[classNum].poll();
-			candidates.add(new Student(classNum, ability));
-			min = Math.min(min, ability);
+			int ability = abilities[n].poll();
+			candidates.offer(new Student(n, ability));
 			max = Math.max(max, ability);
+			min = Math.min(min, ability);
 
 		}
 
@@ -79,31 +78,25 @@ public class Main {
 	}
 
 	public static int solution() {
-
+		
 		int diff = max - min;
 		while (true) {
-
-			// 후보 중 최소 능력치를 가진 학생 추출
+			
 			Student student = candidates.poll();
+			
 			if (abilities[student.classNumber].isEmpty()) {
 				break;
 			}
-
-			// 후보 중 가장 최솟값을 가지는 학생이 속한 반에서 다음 후보가 될 학생을 삽입
+			
 			int ability = abilities[student.classNumber].poll();
 			candidates.offer(new Student(student.classNumber, ability));
-
-			// 후보중 가장 낮은 능력치
+			
 			min = candidates.peek().ability;
-
-			// 다음 후보가 될 학생의 능력치가 가장 큰지 확인
 			max = Math.max(max, ability);
-
-			// 가장 높은 능력치와 가장 낮은 능력치의 차가 더 작다면 갱신
 			diff = Math.min(diff, max - min);
-
+			
 		}
-
+		
 		return diff;
 
 	}
