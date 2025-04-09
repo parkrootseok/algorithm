3745fc669d20bf280812e0270a8a8bcfb32c643a
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-04-09 20:38:06 +0900

    [level 2] Title: 서버 증설 횟수, Time: 19.97 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+// 1. m명마다 서버 1대 추가 증설
+// 2. 증설한 서버는 h ~ h+5 운영
+// 3. 서버 최소 몇 번 증설
+class Solution {
+    
+    static class Server {
+        
+        int start;
+        int end;
+        
+        Server(int start) {
+            this.start = start;
+            this.end = start + K;
+        }
+        
+    }
+    
+    static int M;
+    static int K;
+    
+    public int solution(int[] players, int m, int k) {
+        
+        M = m;
+        K = k;
+        
+        int answer = 0;
+        Queue<Server> servers = new ArrayDeque<>();
+        
+        for (int hour = 0; hour <= 23; hour++) {
+            
+            // 1. 증설된 서버 종료
+            while(!servers.isEmpty()) {
+                // 가장 먼저 증설된 서버의 종료 시각이 현재 시간보다 큰 경우
+                if (hour < servers.peek().end) {
+                    // 반복문 종료
+                    break;
+                }
+                // 그렇지 않으면 현재 서버 종료
+                servers.poll();
+            }
+            
+            
+            // 현재 증설된 서버로
+            int player = players[hour];
+            if (isRunnable(player, servers.size())) {
+                // 실행 가능하면 스킵
+                continue;
+            }
+             
+            // 실행 불가능하면 서버 증설 필요
+            int size = servers.size();
+            int needServer = (int) Math.ceil(player / M);
+            for (int server = 0; server < needServer - size; server++) {
+                answer++;
+                servers.offer(new Server(hour));
+            }
+            
+            System.out.println("[시각:" +  hour + "] 현재 인원 " + player + ", 현재 서버 갯수 " + servers.size());
+            
+        }
+        
+        return answer;
+    }
+    
+    public boolean isRunnable(int player, int size) {
+        // 현재 인원수가 서버*용량 미만이면 실행 가능
+        // 3명 이상 6명 미만
+        return player < (size + 1) * M;
+    }
+    
+}
