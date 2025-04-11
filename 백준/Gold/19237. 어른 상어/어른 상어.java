06ba18856f3d305e61776291b4578a2a98ca23f1
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-04-11 15:05:19 +0900

    [Gold II] Title: 어른 상어, Time: 136 ms, Memory: 16900 KB -BaekjoonHub

--- 백준/Gold/19237. 어른 상어/어른 상어.java
+++ 백준/Gold/19237. 어른 상어/어른 상어.java
@@ -1,15 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * BOJ_어른상어
@@ -17,54 +7,14 @@ import java.util.StringTokenizer;
  */
 public class Main {
 
-	static class Shark {
-		int id;
-		int row;
-		int col;
-		int direction;
-		int[][] priority;
-
-		public Shark(int id, int row, int col) {
-			this.id = id;
-			this.row = row;
-			this.col = col;
-			this.priority = new int[5][5];
-		}
-
-		int getNextDirection(Set<Integer> candidates) {
-			for (int index = 1; index <= 4; index++) {
-				if (candidates.contains(priority[this.direction][index])) {
-					return priority[this.direction][index];
-				}
-			}
-			return 0;
-		}
-
-	}
-
-	static class Smell {
-		int owner;
-		int second;
-
-		public Smell(int owner, int second) {
-			this.owner = owner;
-			this.second = second;
-		}
-	}
+	// 위, 아래, 왼쪽, 오른쪽
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
 
 	static BufferedReader br;
 	static BufferedWriter bw;
 	static StringTokenizer st;
 	static StringBuilder sb;
-
-	static int N;
-	static int M;
-	static int K;
-	static Map<Integer, Shark> sharks;
-	static int[][] grid;
-	static int[][] owner;
-	static int[][] times;
-	static int second;
 
 	public static void main(String[] args) throws IOException {
 
@@ -76,9 +26,29 @@ public class Main {
 		input();
 
 		// 2. 시뮬레이션 수행
-		if (simulation()) {
-			sb.append(second);
-		} else {
+		boolean isPossible = false;
+		for (int second = 1; second <= 1000; second++) {
+			// 1. 냄새 수명 감소
+			decreaseLifeTime();
+
+			// 2. 상어는 현재 위치에 냄새를 남긴다.
+			createSmell();
+
+			// 3. 상어는 이동한다.
+			moveShark();
+
+			// 4. 같은 칸에 여러 상어가 있는지 확인하고, 가장 번호가 낮은 상어만 남긴다.
+			dead();
+
+			// 5. 1번 상어만 남았는지 확인
+			if (!isRunnable()) {
+				isPossible = true;
+				sb.append(second);
+				break;
+			}
+		}
+
+		if (!isPossible) {
 			sb.append(-1);
 		}
 
@@ -87,124 +57,188 @@ public class Main {
 
 	}
 
-	public static boolean simulation() {
+	public static void moveShark() {
 
-		second = 0;
-		while (second++ < 1000) {
-			moveShark();
-			decreaseSmellTime();
-			createSmell();
+		for (int m = 1; m <= M; m++) {
 
-			if (sharks.size() == 1) {
+			// 이미 죽은 상어는 이동 불가
+			if (!sharks[m].isAlive) {
+				continue;
+			}
+
+			boolean isMoved = false;
+			int row = sharks[m].row;
+			int col = sharks[m].col;
+
+			// 현재 방향에 따라 움직이는 방향이 변경됨
+			int[] priority = sharks[m].priority[sharks[m].direction];
+
+			// 1. 냄새가 없는 칸으로 이동
+			for (int dir = 0; dir < dr.length; dir++) {
+
+				int nRow = row + dr[priority[dir]];
+				int nCol = col + dc[priority[dir]];
+
+				if (outRange(nRow, nCol)) {
+					continue;
+				}
+
+				// 냄새가 없는 곳으로 이동
+				if (Objects.isNull(smells[nRow][nCol])) {
+					sharks[m].row = nRow;
+					sharks[m].col = nCol;
+					sharks[m].direction = priority[dir];
+					isMoved = true;
+					break;
+				}
+			}
+
+			if (isMoved) {
+				continue;
+			}
+
+			// 2. 이동하지 못했다면, 우선순위에 따라 자신의 냄새가 있는 칸으로 이동
+			for (int dir = 0; dir < priority.length; dir++) {
+
+				int nRow = row + dr[priority[dir]];
+				int nCol = col + dc[priority[dir]];
+
+				if (outRange(nRow, nCol)) {
+					continue;
+				}
+
+				// 자신의 냄새인 경우 해당 위치로 이동
+				if (!Objects.isNull(smells[nRow][nCol]) && smells[nRow][nCol].index == sharks[m].index) {
+					sharks[m].row = nRow;
+					sharks[m].col = nCol;
+					sharks[m].direction = priority[dir];
+					break;
+				}
+			}
+
+		}
+
+	}
+
+	public static boolean outRange(int row, int col) {
+		return row <= 0 || N < row || col <= 0 || N < col;
+	}
+
+	public static void dead() {
+
+		Shark[][] existSharks = new Shark[N + 1][N + 1];
+
+		for (int m = 1; m <= M; m++) {
+		
+			if (!sharks[m].isAlive) {
+				continue;
+			}
+			
+			int row = sharks[m].row;
+			int col = sharks[m].col;
+
+			// 현재 위치에 이미 상어가 있다면
+			if (!Objects.isNull(existSharks[row][col])) {
+
+				Shark curShark = existSharks[row][col];
+
+				// 두 상어의 번호를 비교하여 더 큰 숫자를 가진 상어는 아웃
+				if (curShark.index < sharks[m].index) {
+					sharks[m].isAlive = false;
+				} else {
+					curShark.isAlive = false;
+					existSharks[row][col] = sharks[m];
+				}
+
+			}
+
+			// 없다면 상어를 추가
+			else {
+				existSharks[row][col] = sharks[m];
+			}
+
+		}
+
+	}
+
+	static class Smell {
+		int index;
+		int limit;
+
+		public Smell(int index, int limit) {
+			this.index = index;
+			this.limit = limit;
+		}
+	}
+
+	public static void createSmell() {
+		for (int m = 1; m <= M; m++) {
+			// 죽은 상어는 스킵
+			if (!sharks[m].isAlive) {
+				continue;
+			}
+
+			// 1. 현재 상어 위치
+			int row = sharks[m].row;
+			int col = sharks[m].col;
+
+			// 2. 냄새를 남긴다.
+			smells[row][col] = new Smell(m, K);
+		}
+	}
+
+	public static void decreaseLifeTime() {
+		for (int row = 1; row <= N; row++) {
+			for (int col = 1; col <= N; col++) {
+				// 현재 위치에 냄새가 있다면
+				if (!Objects.isNull(smells[row][col])) {
+					// 1. 수명 감소
+					smells[row][col].limit--;
+					// 2. 남은 수명이 0이라면 삭제
+					if (smells[row][col].limit == 0) {
+						smells[row][col] = null;
+					}
+				}
+			}
+		}
+	}
+
+	public static boolean isRunnable() {
+		for (int m = 2; m <= M; m++) {
+			// 1. 1번 상외를 제외하고 하나라도 살아있는 상어가 있다면,
+			if (sharks[m].isAlive) {
+				// 진행 가능
 				return true;
 			}
 		}
 
+		// 2. 1번 상어만 살아 있다면, 진행 불가
 		return false;
-
 	}
 
-	public static void moveShark() {
+	static class Shark {
+		int index;
+		int row;
+		int col;
+		int direction;
+		boolean isAlive;
+		int[][] priority;
 
-		// 위, 아래, 왼쪽, 오른쪽
-		int[] dr = {-1, 1, 0, 0};
-		int[] dc = {0, 0, -1, 1};
-		Queue<Integer> outOfGridSharks = new ArrayDeque<>();
-
-		for (int id : sharks.keySet()) {
-
-			Shark shark = sharks.get(id);
-			Set<Integer> noSmell = new HashSet<>();
-			Set<Integer> mySmell = new HashSet<>();
-
-			for (int dir = 0; dir < 4; dir++) {
-
-				int nr = shark.row + dr[dir];
-				int nc = shark.col + dc[dir];
-
-				if (outRange(nr, nc)) {
-					continue;
-				}
-
-				if (times[nr][nc] == 0) {
-					noSmell.add(dir + 1);
-				} else if (owner[nr][nc] == shark.id) {
-					mySmell.add(dir + 1);
-				}
-
-			}
-
-			int nextDirection = shark.getNextDirection(noSmell);
-			if (nextDirection == 0) {
-				nextDirection = shark.getNextDirection(mySmell);
-			}
-
-			// 상어 이동
-			grid[shark.row][shark.col] = 0;
-			if (nextDirection == 1) {
-				shark.row -= 1;
-			} else if (nextDirection == 2) {
-				shark.row += 1;
-			} else if (nextDirection == 3) {
-				shark.col -= 1;
-			} else if (nextDirection == 4) {
-				shark.col += 1;
-			}
-
-			// 이동한 위치에 상어가 없거나, 자신보다 숫자가 크다면
-			if (grid[shark.row][shark.col] == 0 || shark.id < grid[shark.row][shark.col]) {
-				// 이동 가능
-				grid[shark.row][shark.col] = shark.id;
-				shark.direction = nextDirection;
-			}
-
-			// 그렇지 않다면
-			else {
-				// 이동 불가능
-				outOfGridSharks.offer(id);
-			}
-		}
-
-		while (!outOfGridSharks.isEmpty()) {
-			sharks.remove(outOfGridSharks.poll());
-		}
-
-	}
-
-	public static void decreaseSmellTime() {
-		for (int row = 0; row < N; row++) {
-			for (int col = 0; col < N; col++) {
-				if (times[row][col] == 0) {
-					continue;
-				}
-
-				times[row][col]--;
-				if (times[row][col] == 0) {
-					owner[row][col] = 0;
-				}
-			}
-		}
-
-	}
-
-	public static void createSmell() {
-		for (Shark shark : sharks.values()) {
-			owner[shark.row][shark.col] = shark.id;
-			times[shark.row][shark.col] = K;
+		public Shark(int index, int row, int col) {
+			this.index = index;
+			this.row = row;
+			this.col = col;
+			this.isAlive = true;
 		}
 	}
 
-	public static boolean outRange(int row, int col) {
-		return row < 0 || N <= row || col < 0 || N <= col;
-	}
+	static int N;
+	static int M;
+	static int K;
+	static Shark[] sharks;
+	static Smell[][] smells;
 
 	public static void input() throws IOException {
-
-		sharks = new HashMap<>();
-		grid = new int[21][21];
-		owner = new int[21][21];
-		times = new int[21][21];
-
 		// 1. N, M, K 입력
 		// -> 격자 크기, 상어 갯수, 이동 횟수
 		st = new StringTokenizer(br.readLine(), " ");
@@ -213,36 +247,36 @@ public class Main {
 		K = Integer.parseInt(st.nextToken());
 
 		// 2. 격자 정보 입력
-		for (int row = 0; row < N; row++) {
+		smells = new Smell[N + 1][N + 1];
+		sharks = new Shark[M + 1];
+		for (int row = 1; row <= N; row++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			for (int col = 0; col < N; col++) {
+			for (int col = 1; col <= N; col++) {
 				// 2-1. 현재 입력이 0이 아닌 경우, 상어
-				grid[row][col] = Integer.parseInt(st.nextToken());
-				if (0 < grid[row][col]) {
-					sharks.put(grid[row][col], new Shark(grid[row][col], row, col));
-					owner[row][col] = grid[row][col];
-					times[row][col] = K;
+				int number = Integer.parseInt(st.nextToken());
+				if (0 < number) {
+					sharks[number] = new Shark(number, row, col);
 				}
 			}
 		}
 
 		// 3. 현재 방향 정보 받기
 		st = new StringTokenizer(br.readLine(), " ");
-		for (int id = 0; id < M; id++) {
-			sharks.get(id + 1).direction = Integer.parseInt(st.nextToken());
+		for (int index = 1; index <= M; index++) {
+			sharks[index].direction = Integer.parseInt(st.nextToken()) - 1;
 		}
 
 		// 4. 우선 순위 받기
-		for (int id = 0; id < M; id++) {
-			Shark shark = sharks.get(id + 1);
+		for (int m = 1; m <= M; m++) {
+			sharks[m].priority = new int[4][4];
 			for (int dir = 0; dir < 4; dir++) {
 				st = new StringTokenizer(br.readLine(), " ");
-				for (int index = 0; index < 4; index++) {
-					shark.priority[dir + 1][index + 1] = Integer.parseInt(st.nextToken());
+				int index = 0;
+				while (st.hasMoreTokens()) {
+					sharks[m].priority[dir][index++] = Integer.parseInt(st.nextToken()) - 1;
 				}
 			}
 		}
-
 	}
 
 }
