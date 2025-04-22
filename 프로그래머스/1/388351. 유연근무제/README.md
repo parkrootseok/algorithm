978e8e55d2c9965d0dffcab4070d7164a212a554
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-04-22 23:11:48 +0900

    [level 1] Title: 유연근무제, Time: 2.01 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/README.md
+++ 프로그래머스/1/388351. 유연근무제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 유연근무제 - 388351 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351#) 
 
 ### 성능 요약
 
-메모리: 77.3 MB, 시간: 0.91 ms
+메모리: 98.6 MB, 시간: 2.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 22일 23:09:59
+2025년 04월 22일 23:11:46
 
 ### 문제 설명
 
--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,20 +1,18 @@
 class Solution {
+    
     public int solution(int[] schedules, int[][] timelogs, int startday) {
         
         int answer = 0;
-        
-        // 1. 시작날로 부터 일주일 동안 목표 시간에 출근했는지 확인
-        // 2. 단, 토요일, 일요일 출근 시간은 제외
-        
         for (int employee = 0; employee < timelogs.length; employee++) {
             
             int limit = getLimit(schedules[employee]);
             int count = 0;
-            int offset = 0;   
                 
             for (int day = 0; day < timelogs[employee].length; day++) {
                 
-                int curDay = (day + startday) % 7;                
+                int curDay = (day + startday) % 7;
+                
+                // 토요일, 일요일인 경우 검사 제외
                 if (6 != curDay && 0 != curDay) {
                     if (timelogs[employee][day] <= limit){
                         count++;
@@ -27,45 +25,20 @@ class Solution {
                 answer++;
             }
 
-            // System.out.println();
-
         }
  
-        System.out.println(answer);
         return answer;
+        
     }
     
     public static int getLimit(int time) {
-        
         int limit = time + 10;
-        
         if ((limit / 10) % 10 == 6) {
             limit += 100;
             limit -= 60;
         }
         
         return limit;
-        
     }
     
-    public static String convertToString(int day) {
-        
-        switch (day) {
-            case 1:
-                return "월";
-            case 2:
-                return "화";
-            case 3:
-                return "수";
-            case 4:
-                return "목";
-            case 5:
-                return "금";   
-            case 6:
-                return "토";    
-        }
-        
-        return "일";
-        
-    }
 }
