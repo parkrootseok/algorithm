1b92ae26abf9ce862706eb52a53d6f638006421b
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-04-23 11:13:35 +0900

    [level 2] Title: 순위 검색, Time: 992.82 ms, Memory: 201 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -6,92 +6,89 @@ import java.util.*;
  */
 class Solution {
     
-    static final int LANG = 0;
-    static final int JOB = 1;
-    static final int EXP = 2;
-    static final int FOOD = 3;
     static Map<String, List<Integer>> table;
     
     public int[] solution(String[] info, String[] query) {
         
         int[] answer = new int[query.length];
         
-        // table 만들기
-        // -> 가능한 키 조합을 모두 미리 생성
+        // 테이블에 키와 점수 삽입
         table = new HashMap<>();
         for (String i : info) {
-            insert(i.split(" "), 0, "");
+            insert(0, i.split(" "), "");
         }
         
-        // 점수 오름차순 정렬
-        // -> 이후에 오름차순 정렬을 수행하기 위함
+        // 점수는 오름차순 정렬
         for (List<Integer> scores : table.values()) {
             Collections.sort(scores);
         }
-
-				// 쿼리 실행        
-        int index = 0;
-        for (String q : query) {
         
-		        // 쿼리 파싱
-            String[] fields = q.split(" and ");
-            String key = fields[LANG] + fields[JOB] + fields[EXP] + fields[FOOD].split(" ")[0];
+        for (int index = 0; index < query.length; index++) {
             
-            // 키 조합이 없을 경우
-            if (!table.containsKey(key)) {
-                answer[index++] = 0;
-                continue;
+            // 쿼리 파싱
+            String[] conditions = query[index].replace(" and ", " ").split(" ");
+            String key = createKey(conditions);
+            
+            // 키를 포함하고 있다면, 조건에 만족하는 지원자 검색
+            if (table.containsKey(key)) {
+                List<Integer> scores = table.get(key);
+                int rank = binarySearch(scores, Integer.parseInt(conditions[4]));
+                answer[index] = scores.size() - rank;
             }
-            
-            // 이분 탐색(하한) 진행
-            List<Integer> scores = table.get(key);
-            int lowerBoundIndex = binarySearch(scores, Integer.parseInt(fields[FOOD].split(" ")[1]));
-            
-            // 결과 저장
-            answer[index++] = scores.size() - lowerBoundIndex;
+        
         }
         
         return answer;
-    }
-    
-    public void insert(String[] fields, int depth, String key) {
-    
-        if (depth == 4) {
-		        // 조합된 키와 점수 테이블에 추가
-            table
-                .computeIfAbsent(key, k -> new ArrayList<>())
-                .add(Integer.parseInt(fields[depth]));
-                
-            return;
-        }
         
-        // -를 삽입한 경우 
-        insert(fields, depth + 1, key + "-");
-        
-        // fileds값을 삽입한 경우
-        insert(fields, depth + 1, key + fields[depth]);
-    
     }
     
     public int binarySearch(List<Integer> scores, int target) {
         
-        int left = 0;
-        int right = scores.size();
+        int start = 0;
+        int end = scores.size();
         
-        while (left < right) {
-	        
-            int mid = (left + right) >> 1;
+        while (start < end) {
             
-            if (scores.get(mid) < target) {
-		            // 목표값 보다 작다면, 더 큰 범위를 탐색
-                left = mid + 1;
+            int mid = (start + end) >> 1;
+            
+            if (target <= scores.get(mid)) {
+                // 더 작은 범위 탐색
+                end = mid;
             } else {
-		            // target보다 크거나 같다면, 현재 위치를 기록해야 하므로 유지
-                right = mid;
+                // 더 큰 범위 탐색
+                start = mid + 1;
             }
-        } 
+            
+        }
         
-        return right;
+        return end;
+        
+    }
+    
+    public String createKey(String[] conditions) {
+        StringBuilder key = new StringBuilder();
+        
+        key
+            .append(conditions[0])
+            .append(conditions[1])
+            .append(conditions[2])
+            .append(conditions[3]);
+            
+        return key.toString();
+    }
+    
+    public void insert(int depth, String[] columns, String key) {
+        
+        if (depth == columns.length - 1) {
+            table
+                .computeIfAbsent(key, k -> new ArrayList<>())
+                .add(Integer.parseInt(columns[depth]));
+        
+            return;
+        }
+        
+        insert(depth + 1, columns, key + columns[depth]);
+        insert(depth + 1, columns, key + "-");
         
     }
     
