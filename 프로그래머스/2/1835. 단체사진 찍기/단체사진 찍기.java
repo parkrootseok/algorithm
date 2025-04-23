32421ab29f42c47a50fb8197c33064f83e163620
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-04-23 09:57:07 +0900

    [level 2] Title: 단체사진 찍기, Time: 1503.51 ms, Memory: 210 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -5,71 +5,67 @@ import java.util.*;
  * @author parkrootseok
  */
 class Solution {
+    
+    static String[] friends = {"A", "C", "F", "J", "M", "N", "R", "T"};
+    static boolean[] isUsed;
+    static String[] queries;
+    static int answer;
+    
+    public int solution(int n, String[] data) {
+        answer = 0;
+        queries = data;
+        isUsed = new boolean[friends.length];
+        bruteforce(0, new StringBuilder());
+        return answer;
+    }
+    
+    public void bruteforce(int depth, StringBuilder perm) {
+        
+        if (depth == friends.length) {
+             if (isValid(perm.toString())) {
+                 answer++;
+             }
+            return;
+        }
+        
+        for (int index = 0; index < friends.length; index++) {
+            if (!isUsed[index]) {
+                isUsed[index] = true;
+                perm.append(friends[index]);
+                bruteforce(depth + 1, perm);
+                perm.deleteCharAt(perm.length() - 1);
+                isUsed[index] = false;
+            }
+        }
+        
+    }
+    
+    public boolean isValid(String permutation) {
+        
+        for (String query : queries) {
+            
+            int offset = Math.abs(permutation.indexOf(query.charAt(0)) - permutation.indexOf(query.charAt(2))) - 1;
+            char command = query.charAt(3);
+            int condition = query.charAt(4) - '0';
 
-    static char[] friends = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-	static boolean[] isUsed;
-	static String[] conditions;
-	static int answer;
-
-	public int solution(int n, String[] data) {
-		answer = 0;
-		isUsed = new boolean[friends.length];
-		conditions = data;
-		dfs(0, new StringBuilder());
-		System.out.println(answer);
-		return answer;
-
-	}
-
-	public void dfs(int depth, StringBuilder result) {
-
-		if (depth == friends.length) {
-			if (isValid(result.toString())) {
-				answer++;
-			}
-			return;
-		}
-
-		for (int index = 0; index < friends.length; index++) {
-
-			if (!isUsed[index]) {
-				isUsed[index] = true;
-				result.append(friends[index]);
-				dfs(depth + 1, result);
-				result.deleteCharAt(result.length() - 1);
-				isUsed[index] = false;
-			}
-
-		}
-
-	}
-
-	public boolean isValid(String result) {
-
-		for (String condition : conditions) {
-
-			int diff = Math.abs((result.indexOf(condition.charAt(0)) - result.indexOf(condition.charAt(2)))) - 1;
-			char sign = condition.charAt(3);
-			int target = condition.charAt(4) - '0';
-
-			if (sign == '=') {
-				if (target != diff) {
-					return false;
-				}
-			} else if (sign == '>') {
-				if (diff <= target) {
-					return false;
-				}
-			} else {
-				if (diff >= target) {
-					return false;
-				}
-			}
-
-		}
-
-		return true;
-
-	}
-
+            if (command == '=') {
+                if (offset != condition) {
+                    return false;
+                }
+            } else if (command == '>') {
+                if (offset <= condition) {
+                    return false;
+                }
+            } else {
+                if (offset >= condition) {
+                    return false;
+                }
+            }
+            
+        }
+        
+        return true;
+        
+    }
+    
 }
