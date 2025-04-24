f970db10c72c11b6719d4bc7cbd6e2ebaa887c67
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-04-24 17:25:50 +0900

    [Silver IV] Title: 등수 구하기, Time: 68 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -13,7 +13,7 @@ public class Main {
 	static StringBuilder sb;
 
 	static int N;
-	static int newScore;
+	static int S;
 	static int P;
 
 	public static void main(String[] args) throws IOException {
@@ -24,7 +24,7 @@ public class Main {
 
 		st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
-		newScore = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
 		P = Integer.parseInt(st.nextToken());
 
 		if (N == 0) {
@@ -33,30 +33,21 @@ public class Main {
 
 			st = new StringTokenizer(br.readLine(), " ");
 
-			List<Integer> scores = new ArrayList<>();
-			for (int index = 0; index < N; index++) {
-				scores.add(Integer.parseInt(st.nextToken()));
-			}
-
-			scores.add(upperBound(scores, newScore), newScore);
-
-			int curRank = 0;
-			Map<Integer, Integer> rank = new HashMap<>();
-			for (int score : scores) {
-				if (!rank.containsKey(score)) {
-					rank.put(score, ++curRank);
-				} else {
-					curRank++;
+			int rank = 1;
+			int offset = 0;
+			for (int index = 0; index < N && index < P; index++) {
+				int score = Integer.parseInt(st.nextToken());
+				if (S < score) {
+					rank++;
+				} else if (S == score) {
+					offset++;
 				}
 			}
 
-			int diff = scores.lastIndexOf(newScore) - scores.indexOf(newScore);
-			if (P < rank.get(newScore) + diff) {
+			if (P < rank + offset) {
 				sb.append(-1);
-			}
-
-			if (sb.length() == 0) {
-				sb.append(rank.get(newScore));
+			} else {
+				sb.append(rank);
 			}
 
 		}
