3514065459a92f919ebabb5c0bb96e407481f65a
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-07-16 14:18:31 +0900

    [Silver II] Title: 부분수열의 합, Time: 96 ms, Memory: 11672 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * BOJ_부분수열의 합
+ * @author parkrootseok
+ */
+public class Main {
+
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringTokenizer st;
+	static StringBuilder sb;
+
+	static int N, S;
+	static int[] numbers;
+	static int answer;
+
+	public static void main(String[] args) throws IOException {
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+
+		st = new StringTokenizer(br.readLine().trim());
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+
+		numbers = new int[N];
+		st = new StringTokenizer(br.readLine().trim());
+		for (int index = 0; index < N; index++) {
+			numbers[index] = Integer.parseInt(st.nextToken());
+		}
+
+		bruteforce(0, 0, 0);
+
+		sb.append(answer);
+		bw.write(sb.toString());
+		bw.close();
+
+	}
+
+	public static void bruteforce(int depth, int sum, int count) {
+
+		if (depth == N) {
+			if (0 < count && S == sum) {
+				answer++;
+			}
+			return;
+		}
+
+		bruteforce(depth + 1, sum + numbers[depth], count + 1);
+		bruteforce(depth + 1, sum, count);
+
+	}
+
+}
