765957cc9ce65d6cf370e89d72276456652f5753
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-07-29 13:20:53 +0900

    [Gold I] Title: 열쇠, Time: 144 ms, Memory: 18904 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.java
+++ 백준/Gold/9328. 열쇠/열쇠.java
@@ -12,28 +12,15 @@ public class Main {
 	static StringTokenizer st;
 	static StringBuilder sb;
 
-	static class Node {
-		int h;
-		int w;
-
-		public Node(int h, int w) {
-			this.h = h;
-			this.w = w;
-		}
-	}
-
+	static final int[] dh = new int[]{-1, 1, 0, 0};
+	static final int[] dw = new int[]{0, 0, -1, 1};
 	static final char WALL = '*';
-	static final char EMPTY = '.';
 	static final char DOCUMENT = '$';
 
-	static int T;
-	static int H, W;
-	static char[][] BUILDING;
+	static int T, H, W;
+	static char[][] building;
+	static Queue<int[]>[] candidates;
 	static int key;
-	static Queue<Node> entrance;
-	static Map<Integer, Queue<Node>> doors;
-	static boolean[][] isVisited;
-	static int answer;
 
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
@@ -42,137 +29,97 @@ public class Main {
 
 		T = Integer.parseInt(br.readLine());
 		while (0 < T--) {
-
-			st = new StringTokenizer(br.readLine().trim());
+			st = new StringTokenizer(br.readLine());
 			H = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
 
-			BUILDING = new char[H][W];
-			entrance = new ArrayDeque<>();
-			doors = new HashMap<>();
-			key = 0;
-			answer = 0;
-			for (int h = 0; h < H; h++) {
-				BUILDING[h] = br.readLine().toCharArray();
-				for (int w = 0; w < W; w++) {
-					if (BUILDING[h][w] != WALL && isEdge(h, w)) {
-						if (BUILDING[h][w] == DOCUMENT) {
-							BUILDING[h][w] = EMPTY;
-							answer++;
-						} else if (isKey(h, w)) {
-							key |= 1 << (BUILDING[h][w] - 'a');
-						}
-						entrance.offer(new Node(h, w));
-					}
-				}
+			building = new char[H + 2][W + 2];
+			for (int h = 1; h <= H; h++) {
+				char[] inputs = br.readLine().toCharArray();
+				System.arraycopy(inputs, 0, building[h], 1, inputs.length);
 			}
 
+			key = 0;
 			char[] inputs = br.readLine().toCharArray();
 			for (char input : inputs) {
-				if (input != '0') {
-					key |= (1 << (input - 'a'));
+				if (input == '0') {
+					break;
 				}
+				key |= (1 << input - 'a');
 			}
 
-			isVisited = new boolean[H][W];
-			for (Node node : entrance) {
-				int cH = node.h;
-				int cW = node.w;
-				if (isDoor(cH, cW)) {
-					int doorKey = 1 << (BUILDING[cH][cW] - 'A');
-					if (!hasKey(doorKey)) {
-						addWaitingQueue(doorKey, cH, cW);
-						continue;
-					}
-				}
-				bfs(cH, cW);
+			candidates = new Queue[26];
+			for (int index = 0; index < 26; index++) {
+				candidates[index] = new ArrayDeque<>();
 			}
-
-			sb.append(answer).append("\n");
+			sb.append(bfs()).append("\n");
 		}
 
 		bw.write(sb.toString());
 		bw.close();
 	}
 
-	private static void addWaitingQueue(int doorKey, int cH, int cW) {
-		if (doors.containsKey(doorKey)) {
-			doors.get(doorKey).offer(new Node(cH, cW));
-		} else {
-			Queue<Node> queue = new ArrayDeque<>();
-			queue.offer(new Node(cH, cW));
-			doors.put(doorKey, queue);
-		}
-	}
+	public static int bfs() {
+		int answer = 0;
+		boolean[][] isVisited = new boolean[H + 2][W + 2];
+		Queue<int[]> nodes = new ArrayDeque<>();
 
-	public static void bfs(int h, int w) {
-		int[] dh = new int[]{-1, 1, 0, 0};
-		int[] dw = new int[]{0, 0, -1, 1};
-
-		Queue<Node> nodes = new ArrayDeque<>();
-		nodes.offer(new Node(h, w));
-		isVisited[h][w] = true;
-
+		nodes.offer(new int[]{0, 0});
+		isVisited[0][0] = true;
 		while (!nodes.isEmpty()) {
-			Node node = nodes.poll();
+			int[] node = nodes.poll();
+			int cH = node[0];
+			int cW = node[1];
 
 			for (int dir = 0; dir < dh.length; dir++) {
-				int nH = node.h + dh[dir];
-				int nW = node.w + dw[dir];
+				int nH = cH + dh[dir];
+				int nW = cW + dw[dir];
 
-				if (outRange(nH, nW) || isVisited[nH][nW] || BUILDING[nH][nW] == WALL) {
+				if (outRange(nH, nW) || isVisited[nH][nW] || building[nH][nW] == WALL) {
 					continue;
 				}
 
-				if (BUILDING[nH][nW] == DOCUMENT) {
-					answer++;
-				} else if (isKey(nH, nW)) {
-					key |= (1 << BUILDING[nH][nW] - 'a');
-					for (int doorKey : doors.keySet()) {
-						if (hasKey(doorKey)) {
-							extractWatingQueue(doorKey, nodes);
+				if (isKey(nH, nW)) {
+					int keyNumber = building[nH][nW] - 'a';
+					if (!hasKey(keyNumber)) {
+						key |= (1 << keyNumber);
+						while (!candidates[keyNumber].isEmpty()) {
+							nodes.offer(candidates[keyNumber].poll());
 						}
 					}
 				} else if (isDoor(nH, nW)) {
-					int doorKey = 1 << (BUILDING[nH][nW] - 'A');
-					if (!hasKey(doorKey)) {
-						addWaitingQueue(doorKey, nH, nW);
+					int doorNumber = building[nH][nW] - 'A';
+					if (!hasKey(doorNumber)) {
+						candidates[doorNumber].offer(new int[]{nH, nW});
 						continue;
 					}
+				} else if (building[nH][nW] == DOCUMENT) {
+					answer++;
 				}
 
-				nodes.offer(new Node(nH, nW));
+				nodes.offer(new int[]{nH, nW});
 				isVisited[nH][nW] = true;
 			}
 		}
-	}
 
-	private static void extractWatingQueue(int doorKey, Queue<Node> nodes) {
-		Queue<Node> queue = doors.get(doorKey);
-		while (!queue.isEmpty()) {
-			Node door = queue.poll();
-			nodes.offer(new Node(door.h, door.w));
-		}
-	}
-
-	public static boolean hasKey(int doorKey) {
-		return (key & doorKey) == doorKey;
+		return answer;
 	}
 
 	public static boolean outRange(int h, int w) {
-		return h < 0 || H <= h || w < 0 || W <= w;
+		return h < 0 || H + 2 <= h || w < 0 || W + 2 <= w;
+	}
+
+	public static boolean hasKey(int number) {
+		int curBit = 1 << number;
+		return (key & curBit) == curBit;
 	}
 
 	public static boolean isKey(int h, int w) {
-		return 'a' <= BUILDING[h][w] && BUILDING[h][w] <= 'z';
+		return 'a' <= building[h][w] && building[h][w] <= 'z';
 	}
 
 	public static boolean isDoor(int h, int w) {
-		return 'A' <= BUILDING[h][w] && BUILDING[h][w] <= 'Z';
-	}
-
-	public static boolean isEdge(int h, int w) {
-		return h == 0 || h == H - 1 || w == 0 || w == W - 1;
+		return 'A' <= building[h][w] && building[h][w] <= 'Z';
 	}
 
 }
