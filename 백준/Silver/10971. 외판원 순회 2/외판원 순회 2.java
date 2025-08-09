b0ef6e1953ab038d8df632f1be13212168ad770f
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2025-08-09 15:56:58 +0900

    [Silver II] Title: 외판원 순회 2, Time: 72 ms, Memory: 11976 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,112 +1,69 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
+
+import javax.print.DocFlavor;
 
 /**
- * BOJ_10971_외판원순회2
+ * BOJ_외판원순회2
  * @author parkrootseok
- * 
- * - 1 ~ N번 도시가 존재하고, 도시 사이에는 길이 있음
- * - 모든 도시를 거쳐 다시 제자리로 돌아오는 여행 경로를 계획
- * - 단, 한 번 갔던 도로는 다시 올 수 없음
- * 
- * 1. 도시의 수를 입력
- * 2. 각 도시간의 이동 비용을 받는다.
- * 3. 최소 비용을 구한다.
- *  3-1. 이미 방문한 곳이라면 스킵
- *  3-2. 갈 수 없는 도시라면 스킵
- *  3-3. 갈 수 있는 도시라면 비용을 더하여 재귀 호출
- *  3-4. 모든 도시를 다 돌았다면 최소비용 갱신
- **/
-
+ */
 public class Main {
 
 	static BufferedReader br;
 	static BufferedWriter bw;
+	static StringTokenizer st;
 	static StringBuilder sb;
-	static String[] inputs;
-	static String input;
 
-	static int cityNumber;
-	static int[][] cost;
+	static int N;
+	static int[][] W;
+	static int min;
 	static boolean[] isVisited;
-	static int minTotalCost;
 
 	public static void main(String[] args) throws IOException {
-
 		br = new BufferedReader(new InputStreamReader(System.in));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		sb = new StringBuilder();
 
-		// 1. 도시의 수를 입력
-		cityNumber = Integer.parseInt(br.readLine().trim());
-		
-		// 2. 각 도시간 이동 비용을 받는다.
-		cost = new int[cityNumber][cityNumber];
-		for(int row = 0; row < cityNumber; row++) {
-			
-			inputs = br.readLine().trim().split(" ");
-			
-			for(int col = 0; col < cityNumber; col++) {
-				cost[row][col] = Integer.parseInt(inputs[col]);
+		N = Integer.parseInt(br.readLine());
+		W = new int[N + 1][N + 1];
+		for (int r = 1; r <= N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for (int c = 1; c <= N; c++) {
+				W[r][c] = Integer.parseInt(st.nextToken());
 			}
-				
 		}
-		
-		// 3. 최소 비용을 구한다.
-		minTotalCost = Integer.MAX_VALUE;
-		isVisited = new boolean[cityNumber];		
-		for(int start = 0; start < cityNumber; start++) {
-			isVisited[start] = true;
-			dfs(0, start, start, 0);
-		}
-		
-		
-		sb.append(minTotalCost);
+
+		min = Integer.MAX_VALUE;
+		isVisited = new boolean[N + 1];
+        isVisited[1] = true;
+		bruteforce(0, 1, 1, 0);
+	
+		sb.append(min);
 		bw.write(sb.toString());
 		bw.close();
-
 	}
-	
-	public static void dfs(int level, int start, int to, int totalCost) {
-		
-		// 3-4. 모든 도시를 다 돌았다면 최소비용 갱신
-		if (level == cityNumber - 1) {
-			
-			if(cost[to][start] != 0) {
-				minTotalCost = Math.min(minTotalCost, totalCost + cost[to][start]);
+
+	static void bruteforce(int depth, int start, int cCity, int sum) {
+
+		if (depth == N - 1) {
+			if (0 < W[cCity][start]) {
+				min = Math.min(min, sum + W[cCity][start]);
 			}
-	
-			return;
-			
-		}
-		
-		if(totalCost > minTotalCost) {
 			return;
 		}
-		
-		
-		for(int from = 0; from < cityNumber; from++) {
-			
-			// 3-1. 이미 방문한 곳이라면 스킵
-			if (isVisited[from]) {
-				continue;
-			}
-			
-			// 3-2. 갈 수 없는 도시라면 스킵
-			if (cost[to][from] == 0) {
-				continue;
-			}
-			
-			// 3-3. 갈 수 있는 도시라면 비용을 더하여 재귀 호출
-			isVisited[from] = true;
-			dfs(level + 1, start, from, totalCost + cost[to][from]);
-			isVisited[from] = false;
-			
+
+		if (min < sum) {
+			return;
 		}
-			
+
+		for (int nCity = 1; nCity <= N; nCity++) {
+			if (!isVisited[nCity] && 0 < W[cCity][nCity]) {
+				isVisited[nCity] = true;
+				bruteforce(depth + 1, start, nCity, sum + W[cCity][nCity]);
+				isVisited[nCity] = false;
+			}
+		}
+		
 	}
 
 }
